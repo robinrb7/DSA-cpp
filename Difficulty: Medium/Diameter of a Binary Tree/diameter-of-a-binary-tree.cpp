c173ff17d1ee45e8d144798a14a4f0b1c25156f6
Robin Singh Khural <rkhural_be22@thapar.edu>
2024-09-20 16:25:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.cpp
+++ Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.cpp
@@ -92,31 +92,29 @@ struct Node
 }; */
 
 class Solution {
-    private:
-    pair<int,int> diameterFast(Node*root){
+  public:
+    pair<int,int> solve(Node* root){
         if(root==NULL){
-            pair<int,int>p= make_pair(0,0);
+            pair<int,int> p = make_pair(0,0);
             return p;
         }
         
-        pair<int,int>left = diameterFast(root->left);
-        pair<int,int>right = diameterFast(root->right);
+        pair<int,int> left = solve(root->left);
+        pair<int,int> right = solve(root->right);
         
-        int opt1 = left.first;
-        int opt2 = right.first;
-        int opt3 = left.second + right.second +1;
+        int optn1 = left.first;
+        int optn2 = right.first;
+        int optn3 = left.second+right.second+1;
         
         pair<int,int>ans;
-        ans.first = max(opt1,max(opt2,opt3));
-        ans.second = max(left.second,right.second) +1;
-        return ans;
+        ans.first = max(optn1,max(optn2,optn3));
+        ans.second = max(left.second,right.second)+1;
         
+        return ans;
     }
-    
-  public:
-    // Function to return the diameter of a Binary Tree.
     int diameter(Node* root) {
-        return diameterFast(root).first;
+        
+        return solve(root).first;
         
     }
 };
