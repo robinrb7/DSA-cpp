84556c9b5814274774558fcc2da6890e8379b86f
Robin Singh Khural <rkhural_be22@thapar.edu>
2024-10-16 02:13:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
+++ Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
@@ -36,34 +36,46 @@ struct Node
 
 class Solution
 {
-    public:
+    private:
+    
     Node* solve(Node* root,int n1,int n2){
+        
         if(root==NULL){
             return NULL;
         }
-        if(root->data ==n1 || root->data==n2){
+        
+        
+        if(root->data == n1 || root->data==n2){
             return root;
         }
+        
         Node* leftAns = solve(root->left,n1,n2);
         Node* rightAns = solve(root->right,n1,n2);
         
-        if(leftAns!=NULL && rightAns!=NULL){
-            return root;
-        }
-        else if(leftAns==NULL && rightAns!=NULL){
-            return rightAns;
-        }
-        else if(leftAns!=NULL && rightAns==NULL){
-            return leftAns;
-        }
-        else{
+        if(leftAns==NULL && rightAns==NULL){
             return NULL;
         }
+        else if(leftAns != NULL && rightAns==NULL){
+            return leftAns;
+        }
+        else if(leftAns ==NULL && rightAns != NULL){
+            return rightAns;
+        }
+        else{
+            return root;
+        }   
+        
+      
     }
+    
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
     Node* lca(Node* root ,int n1 ,int n2 )
     {
-        Node* ans = solve(root,n1,n2);
-        return ans;
+      if(root==NULL){
+          return NULL;
+      }
+      return solve(root,n1,n2);
     }
 };
 
