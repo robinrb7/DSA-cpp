4ffa57b5b662c79a11534de3d6ebd28b2bbeb204
Robin Singh Khural <rkhural_be22@thapar.edu>
2024-10-20 18:46:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Ancestor in a Tree/kth-ancestor-in-a-tree.cpp
+++ Difficulty: Medium/Kth Ancestor in a Tree/kth-ancestor-in-a-tree.cpp
@@ -112,49 +112,47 @@ struct Node
 */
 // your task is to complete this function
 
-Node* solve(Node* root, int &k, int node){
+Node* solve(Node* root,int &k,int node){
     if(root==NULL){
         return NULL;
     }
+    
     if(root->data == node){
         return root;
     }
     
-    
     Node* leftAns = solve(root->left,k,node);
     Node* rightAns = solve(root->right,k,node);
     
-    if(leftAns!=NULL && rightAns ==NULL){
+    if(leftAns!= NULL && rightAns==NULL){
+      k--;
+        
+        if(k==0){
+            k=INT_MAX;
+            return root;
+        }
+        return leftAns;   
+    }
+    else if(leftAns==NULL && rightAns!=NULL){
         k--;
         
         if(k==0){
+            k=INT_MAX;
             return root;
         }
-        
-         return leftAns;
+        return rightAns;
     }
-    else if(leftAns==NULL && rightAns != NULL){
-        k--;
-        
-        if(k==0){
-            return root;
-        }
-        
-         return rightAns;
-    }
+    
     return NULL;
-    
-    
 }
+
 int kthAncestor(Node *root, int k, int node)
 {
-    if(root==NULL ){
-        return -1;
-    }
-    
     Node* ans = solve(root,k,node);
-    if(ans->data == node){
+    if(ans->data ==node){
         return -1;
     }
-    return ans->data;
+    else{
+        return ans->data;
+    }
 }
