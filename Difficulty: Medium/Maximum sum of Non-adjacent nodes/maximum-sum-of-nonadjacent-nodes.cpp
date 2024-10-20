0f1b854ebaf9cdec66b8e73ce5c642a4bba11522
Robin Singh Khural <rkhural_be22@thapar.edu>
2024-10-20 16:27:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum sum of Non-adjacent nodes/maximum-sum-of-nonadjacent-nodes.cpp
+++ Difficulty: Medium/Maximum sum of Non-adjacent nodes/maximum-sum-of-nonadjacent-nodes.cpp
@@ -102,30 +102,29 @@ struct Node
 */
 
 class Solution{
-  public:
+    private:
     pair<int,int> solve(Node* root){
         if(root==NULL){
             pair<int,int>p = make_pair(0,0);
             return p;
         }
-        pair<int,int> ans;
         
         pair<int,int> leftAns = solve(root->left);
         pair<int,int> rightAns = solve(root->right);
         
+        pair<int,int>ans;
         ans.first = root->data + leftAns.second + rightAns.second;
         ans.second = max(leftAns.first,leftAns.second) + max(rightAns.first,rightAns.second);
         
         return ans;
     }
+  public:
+    //Function to return the maximum sum of non-adjacent nodes.
     int getMaxSum(Node *root) 
     {
-        if(root==NULL){
-            return 0;
-        }
-        pair<int,int> res = solve(root);
-        return max(res.first,res.second);
+        pair<int,int>ans =solve(root);
         
+        return max(ans.first,ans.second);
     }
 };
 
