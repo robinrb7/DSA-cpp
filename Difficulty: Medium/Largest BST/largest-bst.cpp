0735aa183d690597890dc47dbc837673b2047084
Robin Singh Khural <rkhural_be22@thapar.edu>
2024-11-01 00:43:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.cpp
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -101,54 +101,46 @@ struct Node {
 
 class Solution{
     class info{
-        
         public:
         
-        int mini;
+        int mini;;
         int maxi;
         bool isBST;
         int size;
     };
     private:
-    info solve(Node* root, int &ans){
+    info solve(Node* root, int& maxSize){
         if(root==NULL){
             return {INT_MAX,INT_MIN,true,0};
         }
         
-        info left = solve(root->left,ans);
-        info right = solve(root->right,ans);
+        info left = solve(root->left,maxSize);
+        info right = solve(root->right,maxSize);
         
         info currNode;
-        currNode.mini = min(left.mini,root->data);
-        currNode.maxi = max(right.maxi,root->data);
+        currNode.mini = min(root->data,left.mini);
+        currNode.maxi = max(root->data, right.maxi);
         currNode.size = left.size + right.size +1;
         
-        if(left.isBST && right.isBST 
-           && (root->data > left.maxi && root->data < right.mini)){
-            
+        if(left.isBST && right.isBST && (root->data > left.maxi && root->data < right.mini)){
             currNode.isBST = true;
         }
-        else{
-            currNode.isBST = false;
-        }
+        else currNode.isBST = false;
         
         if(currNode.isBST){
-            ans = max(ans,currNode.size);
+            maxSize = max(maxSize,currNode.size);
         }
         
         return currNode;
-        
     }
     public:
     /*You are required to complete this method */
     // Return the size of the largest sub-tree which is also a BST
     int largestBst(Node *root)
     {
-    	int ans =0;
-    	
-    	info temp = solve(root,ans);
-    	
-    	return ans;
+    	int maxSize =0;
+    	solve(root,maxSize);
+    	return maxSize;
     }
 };
 
