d06b22e5591bb8c7ba5df2116cab7625ee024e4d
Robin Singh Khural <rkhural_be22@thapar.edu>
2024-11-07 17:30:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k largest elements/k-largest-elements.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    vector<int> kLargest(vector<int>& arr, int k) {
+       priority_queue<int> maxHeap;
+       
+       for(int i =0;i<arr.size();i++){
+           maxHeap.push(arr[i]);
+       }
+       
+       vector<int>ans;
+       while(k--){
+           ans.push_back(maxHeap.top());
+           maxHeap.pop();
+       }
+       
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.kLargest(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
