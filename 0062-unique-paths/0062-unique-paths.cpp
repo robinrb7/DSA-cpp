0c595fa6a946899f26b571603cf7b0869e17a1fe
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-04-01 04:39:20 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,54 +1,14 @@
 class Solution {
-    private:
-    int solve(int n,int m, int i, int j,vector<vector<int>>&dp){
-        if(i==n-1 && j==m-1) return 1;
-        if(i>=n || j>=m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int down = solve(n,m,i+1,j,dp);
-        int right = solve(n,m,i,j+1,dp);
-
-        dp[i][j] = down+right;
-        return dp[i][j];
-    }
-    int solveTab(int n,int m){
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        dp[n-1][m-1]=1;
-
-        for(int i = n-1;i>=0;i--){
-            for(int j =m-1;j>=0;j--){
-                if(i==n-1 && j==m-1) continue;
-                int down = dp[i+1][j];
-                int right = dp[i][j+1];
-
-                dp[i][j] = down+right;
-            }
-        }
-        return dp[0][0];
-        
-    }
-
-    int solveOptm(int n,int m){
-        vector<int>curr(m+1,0);
-        vector<int>next(m+1,0);
-        curr[m-1]=1;
-
-        for(int i = n-1;i>=0;i--){
-            for(int j =m-1;j>=0;j--){
-                if(i==n-1 && j==m-1) continue;
-                int down = next[j];
-                int right = curr[j+1];
-
-                curr[j] = down+right;
-            }
-            next =curr;
-        }
-        return curr[0];
-        
-    }
 public:
     int uniquePaths(int m, int n) {
-        
-        return solveOptm(m,n);
+        int N = m+n-2;
+        int r = min(m-1,n-1);
+        long long ans = 1;
+
+        for(int i =0;i<r;i++){
+            ans = ans* (N-i)/(i+1);
+        }
+
+        return (int)ans;
     }
 };
