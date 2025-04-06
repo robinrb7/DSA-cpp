d4c12f051c3dd12fa71c74aea26ff272b1516f38
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-04-06 23:38:11 +0530

    Time: 10 ms (46.48%), Space: 11.3 MB (53.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -13,18 +13,19 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast){
+        while(fast->next && fast->next->next){
             slow= slow->next;
-            fast=fast->next;
-            if(fast) fast=fast->next;
-            if(slow==fast) break;
+            fast=fast->next->next;
+
+            if(slow==fast){
+                slow = head;
+                while(slow!=fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
-        if(fast==NULL) return NULL;
-        slow = head;
-        while(slow!=fast){
-            slow = slow->next;
-            fast = fast->next;
-        }
-        return slow;
+        return NULL;
     }
 };
