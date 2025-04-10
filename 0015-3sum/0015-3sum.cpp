ae3e611273ed4940df8667a33c2ca3595c3e1b67
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-04-10 14:16:00 +0530

    Time: 51 ms (55.92%), Space: 31.1 MB (26.65%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>ans;
+        int n= nums.size();
         sort(nums.begin(),nums.end());
-        int i=0;
+        vector<vector<int>> ans;
 
+        int i =0;
         while(i<n){
-            int j=i+1,k=n-1;
+            int j = i+1,k=n-1;
             while(j<k){
-                int element = nums[i]+nums[j]+nums[k];
+                int element  = nums[i]+nums[j]+nums[k];
                 if(element==0){
-                    vector<int>temp = {nums[i],nums[j],nums[k]};
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
                     ans.push_back(temp);
 
-                    int y=nums[j],z=nums[k];
-                    while(j<k && nums[j]==y)j++;
-                    while(j<k && nums[k]==z)k--;
+                    int y =nums[j],x=nums[k];
+                    while(j<k && nums[j]==y) j++;
+                    while(j<k && nums[k]==x) k--;
                 }
                 else if(element<0) j++;
                 else k--;
             }
-            int x =nums[i];
-            while(i<n && nums[i]==x)i++;
+            int z = nums[i];
+            while(i<n && nums[i]==z)i++;
         }
 
         return ans;
