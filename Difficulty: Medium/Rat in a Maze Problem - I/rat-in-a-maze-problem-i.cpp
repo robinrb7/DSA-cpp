b0da59f25b8b6ba2e836fc3cc84a89d6229279cf
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-06-13 15:35:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,129 +1,53 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
     private:
-    bool isSafe(int newx,int newy,vector <vector<bool> >&visited,vector<vector<int>> &mat,int n){
-        if((newx>=0 && newx<n) && (newy>=0 && newy<n) && visited[newx][newy]!=1 
-            && mat[newx][newy]!=0){
-                return true;
-            }
-        else return false;    
-    }
-    
-    void solve(int x,int y,vector<vector<int>> &mat,vector<string>&ans,
-               string path,vector<vector<bool> >&visited,int n)
-    {
-        if(x==n-1 && y==n-1){
-            ans.push_back(path);
+    void solve(vector<vector<int>>& maze, vector<string> &ans, string &temp,int n, int row, int col,vector<vector<int>> &visited){
+        if(row==n-1 && col == n-1){
+            ans.push_back(temp);
             return;
         }
         
-        //DLRU
-        visited[x][y]=1;
-        //Down
-        if(isSafe(x+1,y,visited,mat,n)){
-            solve(x+1,y,mat,ans,path+"D",visited,n);
+        visited[row][col] =1;
+        
+        if( row+1 <n && col<n && maze[row+1][col]==1 && visited[row+1][col] !=1){
+            temp += 'D';
+            solve(maze,ans,temp,n,row+1,col,visited);
+            temp.pop_back();
         }
         
-        //Left
-        if(isSafe(x,y-1,visited,mat,n)){
-            solve(x,y-1,mat,ans,path+"L",visited,n);
+        if ( row <n && col-1>=0 && maze[row][col-1]==1 && visited[row][col-1] !=1){
+            temp += 'L';
+            solve(maze,ans,temp,n,row,col-1,visited);
+            temp.pop_back();
         }
         
-        //Right
-        if(isSafe(x,y+1,visited,mat,n)){
-            solve(x,y+1,mat,ans,path+"R",visited,n);
+        if(row <n && col+1<n && maze[row][col+1]==1 && visited[row][col+1] !=1){
+            temp += 'R';
+            solve(maze,ans,temp,n,row,col+1,visited);
+            temp.pop_back();
         }
         
-        //Up
-        if(isSafe(x-1,y,visited,mat,n)){
-            solve(x-1,y,mat,ans,path+"U",visited,n);
+        if(row-1 >=0 && col<n && maze[row-1][col]==1 && visited[row-1][col] !=1){
+            temp += 'U';
+            solve(maze,ans,temp,n,row-1,col,visited);
+            temp.pop_back();
         }
         
-        visited[x][y]=0;
+        visited[row][col] =0;
     }
-    
   public:
-    vector<string> findPath(vector<vector<int>> &mat) {
-        vector<string> ans;
-        if(mat[0][0]!=1) return ans;
+    vector<string> ratInMaze(vector<vector<int>>& maze) {
+        vector<string>ans;
+        if(maze[0][0]==0) return ans;
         
-        string path="";
-        int rows  = mat.size();
-        int cols = mat[0].size();
+        int n = maze.size();
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        string temp = "";
+        int row =0;
+        int col =0;
         
-        vector<vector<bool> > visited(rows,vector<bool>(cols,0));
-        
-        solve(0,0,mat,ans,path,visited,rows);
+        solve(maze,ans,temp,n,row,col,visited);
         
         return ans;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        string input;
-        getline(cin, input);
-        vector<vector<int>> mat;
-        string innerArray;
-        bool isInsideArray = false;
-
-        for (char c : input) {
-            if (c == '[') {
-                if (isInsideArray) {
-                    innerArray.clear();
-                }
-                isInsideArray = true;
-            } else if (c == ']') {
-                if (isInsideArray && !innerArray.empty()) {
-                    vector<int> row;
-                    stringstream ss(innerArray);
-                    int num;
-
-                    while (ss >> num) {
-                        row.push_back(num);
-                        if (ss.peek() == ',')
-                            ss.ignore();
-                        while (isspace(ss.peek()))
-                            ss.ignore();
-                    }
-
-                    mat.push_back(row);
-                    innerArray.clear();
-                }
-                isInsideArray = false;
-            } else if (isInsideArray) {
-                if (!isspace(c)) {
-                    innerArray += c;
-                }
-            }
-        }
-
-        Solution obj;
-        vector<string> result = obj.findPath(mat);
-        sort(result.begin(), result.end());
-
-        if (result.empty())
-            cout << "[]";
-        else
-            for (int i = 0; i < result.size(); i++)
-                cout << result[i] << " ";
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
