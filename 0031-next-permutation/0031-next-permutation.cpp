06413d466546caa0c1ad87c63d149de7b95938ac
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-06-23 19:24:46 +0530

    Time: 1 ms (4.84%), Space: 15.7 MB (67.54%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,40 +1,28 @@
 class Solution {
-    private:
-    int justGreater(vector<int>&nums, int start, int n){
-        int pivot = nums[start-1];
-        int mini = INT_MAX;
-        int minIndx = -1;
-        for(int i = start;i<n;i++){
-            if(nums[i]>pivot && nums[i]<=mini){
-                mini = nums[i];
-                minIndx = i;
-            }
-        }
-        return minIndx;
-    }
-
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int pivot = -1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                pivot =i;
+        if(n==1) return;
+
+        int index=-1;
+        for(int i = n-2;i>=0;i--){
+            if(nums[i+1]>nums[i]){
+                index=i;
                 break;
             }
         }
-        if(pivot==-1){
+        if(index==-1){
             reverse(nums.begin(),nums.end());
             return;
         }
 
-        int nextPivot = justGreater(nums,pivot+1,n);
-        swap(nums[pivot],nums[nextPivot]);
-
-        int i = pivot+1, j=n-1;
-        while(i<=j){
-            swap(nums[i++],nums[j--]);
+        for(int i= n-1;i>index;i--){
+            if(nums[i] > nums[index]){
+                swap(nums[index],nums[i]);
+                break;
+            }
         }
+        reverse(nums.begin() + index+1,nums.end());
 
         return;
     }
