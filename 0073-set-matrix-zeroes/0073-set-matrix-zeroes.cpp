94e1410c4e72f0aba517ac752ee1a47f6372d9d2
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-06-23 22:53:47 +0530

    Time: 0 ms (100%), Space: 18.7 MB (50.96%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,24 +4,32 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int> row(n,0);
-        vector<int> col(m,0);
+        int col_0 = 1;
+        if(matrix[0][0]==0) col_0=0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    matrix[i][0]=0;
+                    if(j==0) col_0=0;
+                    else matrix[0][j]=0;
                 }
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1 || col[j]==1){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
+        }
+
+        if(matrix[0][0]==0){
+            for(int i=0;i<m;i++) matrix[0][i]=0;
+        }
+        if(col_0==0){
+            for(int i=0;i<n;i++) matrix[i][0]=0;
         }
 
         return;
