f468c38e4d1340cb5fb75cf21999bc26be16435f
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-01 17:30:40 +0530

    Time: 0 ms (100%), Space: 13.4 MB (97.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-        int s=0,e=n-1;
-        while(s<=e){
-            int mid = s +(e-s)/2;
+        int n= nums.size();
+        int low =0,high=n-1;
 
-            if(nums[mid]==target) return mid;
-            if(nums[mid]<target) s=mid+1;
-            else e =mid-1;
+        while(low<=high){
+            int mid = low +(high-low)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else high = mid-1;
         }
-        return e+1;
+
+        return low;
     }
 };
