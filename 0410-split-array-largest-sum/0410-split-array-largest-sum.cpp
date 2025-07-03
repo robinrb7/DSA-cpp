8b17c1dbf579e3dde9c67b503bb165722ce6e3d0
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-03 20:17:46 +0530

    Time: 0 ms (100%), Space: 10.5 MB (23.44%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
     private:
-    int countArr(vector<int>&nums, int k, int maxi){
+    bool poss(vector<int>&nums, int k, int maxi){
         int n = nums.size();
         int sum=0, cnt=1;
         for(int i =0;i<n;i++){
-            if(nums[i]>maxi) return 1e9;
+            if(nums[i]>maxi) return 0;
             if(sum + nums[i] > maxi){
                 cnt++;
                 sum=0;
             }
             sum += nums[i];
+            if(cnt>k) return 0;
         }
-        return cnt;
+        return 1;
     }
 public:
     int splitArray(vector<int>& nums, int k) {
         int n= nums.size();
+        if(k>n) return -1;
         int mini= INT_MAX, sum=0;
         for(int i =0;i<n;i++){
             if(nums[i]<mini) mini=nums[i];
@@ -26,8 +28,7 @@ public:
         while(low<=high){
             int mid = low + (high-low)/2;
 
-            int val = countArr(nums,k,mid);
-            if(val <= k){
+            if(poss(nums,k,mid)){
                 high = mid-1;
             }
             else low = mid+1;
