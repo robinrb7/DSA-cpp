f408f4dfebec96670310d966586b55e038edacef
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-05 18:18:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,41 +1,39 @@
 // User function template for C++
 
 class Solution {
-    private:
-    int upperB(vector<int>arr,int mid){
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]>mid) return i;
+    int func(vector<vector<int>> &mat,int num){
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        int cnt=0;
+        for(int i =0;i<n;i++){
+            int index = upper_bound(mat[i].begin(),mat[i].end(),num) - mat[i].begin();
+            cnt += index;
         }
         
-        return arr.size();
+        return cnt;
     }
-    int getSmall(vector<vector<int>> &mat, int mid){
-        int ans = 0;
-        for(int i=0;i<mat.size();i++){
-            ans += upperB(mat[i],mid);
-        }
-        
-        return ans;
-    }
-    
   public:
     int median(vector<vector<int>> &mat) {
-        int n = mat.size(), m  = mat[0].size();
-        int low = INT_MAX, high = INT_MIN;
+        int n = mat.size();
+        int m = mat[0].size();
         
+        int mini= INT_MAX, maxi=INT_MIN;
         for(int i=0;i<n;i++){
-            if(mat[i][0] < low) low = mat[i][0];
-            if(mat[i][m-1] > high) high = mat[i][m-1];
+            if(mat[i][0]<mini) mini = mat[i][0];
+            if(mat[i][m-1]>maxi) maxi = mat[i][m-1];
         }
         
-        
-        int total = (m*n)/2;
+        int low = mini, high = maxi;
+        int numLeft = (n*m)/2;
         while(low<=high){
-            int mid = low + (high-low)/2;
-            int smallerEqual = getSmall(mat,mid);
+            int mid = low +(high-low)/2;
             
-            if(smallerEqual <= total) low = mid+1;
-            else high = mid -1;
+            int cnt = func(mat,mid);
+            if(cnt >numLeft){
+                high = mid-1;
+            }
+            else low = mid+1;
         }
         return low;
         
