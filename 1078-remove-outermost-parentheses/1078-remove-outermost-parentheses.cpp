052cab8315f778a34864b8569ecfbf6d1a7394b6
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-05 22:27:54 +0530

    Time: 0 ms (100%), Space: 9 MB (24.61%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         int n=s.length();
-        stack<int>st;
+        int cnt=0;
         
         string ans="";
         for(int i=0;i<n;i++){
             if(s[i]=='('){
-                if(!st.empty()) ans+= s[i];
-                st.push(s[i]);
+                if(cnt!=0) ans+= s[i];
+                cnt++;
             }
             else{
-                st.pop();
-                if(!st.empty()) ans+= s[i];
+                cnt--;
+                if(cnt!=0) ans+= s[i];
             }
         }
         
