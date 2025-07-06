5552af30f46e2da1c94d937cddc2da477ba40341
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-06 10:29:58 +0530

    Time: 0 ms (100%), Space: 11.8 MB (66.62%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,22 +3,17 @@ public:
     string longestCommonPrefix(vector<string>& str) {
         int n = str.size();
 
-        int minLen = str[0].size();
+        string prefix = str[0];
         for (int i = 1; i < n; i++) {
-            if (str[i].length() < minLen) minLen = str[i].length();
-        }
-
-        for (int i = 0; i < minLen; i++) {
-            char ch = str[0][i];
-            for (int j = 1; j < n; j++) {
-                if (str[j][i] != ch) {
-                    minLen = i;
-                    break;
+            while(str[i].find(prefix) !=0){
+                prefix.pop_back();
+                if(prefix.empty()){
+                    return prefix;
                 }
             }
         }
 
-        string ans = str[0].substr(0, minLen);
-        return ans;
+        return prefix;
+
     }
 };
