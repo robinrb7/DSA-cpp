4ca46d887f58c35230e3f0696284a2d0c7d2088a
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-07 04:52:26 +0530

    Time: 7 ms (21.83%), Space: 17 MB (7.42%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,24 +1,26 @@
 class Solution {
-    private:
-    bool static cmp(pair<char,int>&a, pair<char,int>&b){
-        return a.second > b.second;
-    }
+
 public:
     string frequencySort(string s) {
         int n = s.length();
         unordered_map<char,int> hash;
-        priority_queue<pair<int,char>>pq;
 
         for(int i =0;i<n;i++){
             hash[s[i]]++;
         }
 
-        vector<pair<char,int>> charFreq(hash.begin(),hash.end());
-        sort(charFreq.begin(),charFreq.end(),cmp);
+        vector<vector<char>> bucket(n+1);
+        for(auto it :hash){
+            char ch = it.first;
+            int freq = it.second;
+            bucket[freq].push_back(ch);
+        }
 
         string result = "";
-        for(auto it:charFreq){
-            result.append(it.second,it.first);
+        for(int i=n;i>=1;i--){
+            for(auto ch: bucket[i]){
+                result.append(i,ch);
+            }
         }
 
         return result;
