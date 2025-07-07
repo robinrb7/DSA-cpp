1d140c028f7860ea3d3c41f36ae92f9ab03b355a
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-07 18:53:55 +0530

    Time: 0 ms (100%), Space: 10 MB (93.39%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,26 +1,27 @@
 class Solution {
     private:
-    void insertVal(unordered_map<char,int> &val){
-        val['I']=1;
-        val['V']=5;
-        val['X']=10;
-        val['L']=50;
-        val['C']=100;
-        val['D']=500;
-        val['M']=1000;
+    int val(char ch){
+        switch(ch){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        
+        return 0;
     }
 public:
     int romanToInt(string s) {
        int n= s.length();
 
-       unordered_map<char,int> val;
-       insertVal(val);
-
        int i=n-2;
-       int ans= val[s[n-1]];
+       int ans = val(s[n-1]);
        while(i>=0){
-            if(val[s[i]]<val[s[i+1]]) ans -= val[s[i]];
-            else ans += val[s[i]];
+            if(val(s[i])<val(s[i+1])) ans -= val(s[i]);
+            else ans += val(s[i]);
             i--;
        }
 
