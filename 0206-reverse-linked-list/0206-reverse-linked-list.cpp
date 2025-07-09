f933fb21542e3907adab028213fd2694c3d35ac7
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-09 21:20:54 +0530

    Time: 0 ms (100%), Space: 13.8 MB (10.52%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,22 +10,19 @@
  */
 class Solution {
     private:
+    ListNode* revLink(ListNode* head){
+        if(head->next==NULL) return head;
+
+        ListNode* newHead = revLink(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+    }
 
 public:
     ListNode* reverseList(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
 
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        ListNode* forw = NULL;
-        while(curr){
-            forw = curr->next;
-
-            curr->next = prev;
-            prev = curr;
-            curr=forw;
-        }
-
-        return prev;
+        return revLink(head);
     }
 };
