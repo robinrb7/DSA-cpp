18e40249c3720804b11f7cf0116f5aa32d5b2b91
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-13 18:43:51 +0530

    Time: 0 ms (100%), Space: 10.2 MB (5.47%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,4 +1,14 @@
 class Solution {
+    private:
+    void solve(string s, int index, int n, long long &ans,bool negSign){
+        if(index==n || (s[index]<'0' || s[index] > '9')) return;
+
+        if(!negSign && ans >= INT_MAX) return;
+        if(negSign && -ans <= INT_MIN) return;
+
+        ans = ans*10 + (s[index]-'0');
+        solve(s, index+1,n,ans,negSign);
+    }
 public:
     int myAtoi(string s) {
         int n=s.length();
@@ -12,14 +22,10 @@ public:
 
         while(i<n && s[i]=='0')i++;
         long long ans = 0;
-        while(s[i] >='0' && s[i]<='9'){
-            int num = s[i]-'0';
-            ans = ans*10 + num;
-            if(!negSign && ans >= INT_MAX) return INT_MAX;
-            if(negSign && -ans <= INT_MIN) return INT_MIN;
+        solve(s,i,n,ans,negSign);
 
-            i++;
-        }
+        if(!negSign && ans >= INT_MAX) return INT_MAX;
+        if(negSign && -ans <= INT_MIN) return INT_MIN;
 
         if(negSign) ans = -ans;
         return ans;
