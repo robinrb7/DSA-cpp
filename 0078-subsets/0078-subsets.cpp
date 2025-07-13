b05ded7edc721c847e81b091637e48c9ca84f350
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-14 01:13:15 +0530

    Time: 3 ms (32.62%), Space: 15.5 MB (21.65%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    void solve(vector<int>& nums,vector<int> temp,vector<vector<int>> &res, int index, int n){
+        if(index==n) {
+            res.push_back(temp);
+            return;
+        }
+
+        temp.push_back(nums[index]);
+        solve(nums,temp,res,index+1,n);
+        temp.pop_back();
+
+        solve(nums,temp,res,index+1,n);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n=nums.size();
+        if(n==0) return res;
+
+        vector<int>temp;
+        solve(nums,temp,res,0,n);
+        return res;
+    }
+};
