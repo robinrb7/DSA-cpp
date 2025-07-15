20dda1693e8f1fc64fc10f995b334277b33783bc
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-15 21:59:52 +0530

    Time: 3 ms (41.25%), Space: 15.4 MB (17.56%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,32 @@
 class Solution {
     private:
-    void solve(vector<int>& cand, int target,vector<vector<int>>&ans,vector<int>&temp,int index, int n){
+    void solve(vector<int>& candidates, int target,vector<vector<int>> &res,vector<int> temp, int n, int index){
         if(target==0){
-            ans.push_back(temp);
+            res.push_back(temp);
             return;
         }
-        if(index>=n || target<0) return;
+        if(index==n || target<0) return;
 
-        for(int i = index;i<n;i++){
-            if(i!=index && cand[i]==cand[i-1]) continue;
-            
-            temp.push_back(cand[i]);
-            solve(cand,target-cand[i],ans,temp,i+1,n);
+
+        for(int i =index;i<n;i++){
+            if(i!= index && candidates[i]== candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+
+            temp.push_back(candidates[i]);
+            solve(candidates,target-candidates[i],res,temp,n,i+1);
             temp.pop_back();
         }
     }
+
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int>temp;
         int n= candidates.size();
-
         sort(candidates.begin(),candidates.end());
-        solve(candidates,target,ans,temp,0,n);
-        return ans;
+        vector<vector<int>> res;
+        vector<int> temp;
+
+        solve(candidates,target,res,temp,n,0);
+
+        return res;
     }
 };
