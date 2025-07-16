d0e80c6acfebd6aa61d7ce939e91056ccd21cb29
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-16 18:57:23 +0530

    Time: 814 ms (28.94%), Space: 11.8 MB (24.34%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,18 +1,19 @@
 class Solution {
-    bool solve(vector<vector<char>>& board, string word,vector<vector<bool>> &visited,int stringIndex, int rowIndex,int colIndex, int row , int col){
+    bool solve(vector<vector<char>>& board, string word,int stringIndex, int rowIndex,int colIndex, int row , int col){
         if(stringIndex==word.length()) return true;
-        if(rowIndex<0 || rowIndex==row || colIndex==col || colIndex<0 || board[rowIndex][colIndex]!=word[stringIndex] || visited[rowIndex][colIndex] ==1){
+        if(rowIndex<0 || rowIndex==row || colIndex==col || colIndex<0 || board[rowIndex][colIndex]!=word[stringIndex] || board[rowIndex][colIndex] =='$'){
             return false;
         } 
         
-        visited[rowIndex][colIndex]=1;
+        char temp = board[rowIndex][colIndex];
+        board[rowIndex][colIndex]='$';
 
-        bool right = solve(board,word,visited,stringIndex+1,rowIndex,colIndex+1,row,col);
-        bool left = solve(board,word,visited,stringIndex+1,rowIndex,colIndex-1,row,col);
-        bool down = solve(board,word,visited,stringIndex+1,rowIndex+1,colIndex,row,col);
-        bool up =   solve(board,word,visited,stringIndex+1,rowIndex-1,colIndex,row,col);
+        bool right = solve(board,word,stringIndex+1,rowIndex,colIndex+1,row,col);
+        bool left = solve(board,word,stringIndex+1,rowIndex,colIndex-1,row,col);
+        bool down = solve(board,word,stringIndex+1,rowIndex+1,colIndex,row,col);
+        bool up =   solve(board,word,stringIndex+1,rowIndex-1,colIndex,row,col);
 
-        visited[rowIndex][colIndex]=0;
+        board[rowIndex][colIndex]= temp;
         return (right || left || down || up);
     }
 public:
@@ -20,11 +21,9 @@ public:
         int nRow = board.size();
         int nCol = board[0].size();
 
-        vector<vector<bool>> visited(nRow,vector<bool>(nCol,0));
-
         for(int i =0;i<nRow;i++){
             for(int j=0;j<nCol;j++){
-                if(solve(board,word,visited,0,i,j,nRow,nCol)) return 1;
+                if(solve(board,word,0,i,j,nRow,nCol)) return 1;
             }
         }
        return 0; 
