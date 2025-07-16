d2316b6e4640c9f68443b2793f1b91a0d69cf2de
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-16 22:45:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,53 +1,44 @@
 class Solution {
     private:
-    void solve(vector<vector<int>>& maze, vector<string> &ans, string &temp,int n, int row, int col,vector<vector<int>> &visited){
-        if(row==n-1 && col == n-1){
+     void solve(vector<vector<int> > &grid,vector<string> &ans,string temp, int row, int col,int nRow, int nCol){
+        if(row<0 || row==nRow || col<0 || col==nCol || grid[row][col]==0){
+            return;
+        }
+        
+        if(row==nRow-1 && col==nCol-1){
             ans.push_back(temp);
             return;
         }
         
-        visited[row][col] =1;
+
+        grid[row][col]=0;
+
+        //MOVE UP
+        solve(grid,ans,temp+'U',row-1,col,nRow,nCol);
+
+        //MOVE DOWN
+        solve(grid,ans,temp+'D',row+1,col,nRow,nCol);
+
+        //MOVE LEFT
+        solve(grid,ans,temp+'L',row,col-1,nRow,nCol);
+
+        //MOVE RIGHT
+        solve(grid,ans,temp+'R',row,col+1,nRow,nCol);
+
+        grid[row][col]=1;
         
-        if( row+1 <n && col<n && maze[row+1][col]==1 && visited[row+1][col] !=1){
-            temp += 'D';
-            solve(maze,ans,temp,n,row+1,col,visited);
-            temp.pop_back();
-        }
-        
-        if ( row <n && col-1>=0 && maze[row][col-1]==1 && visited[row][col-1] !=1){
-            temp += 'L';
-            solve(maze,ans,temp,n,row,col-1,visited);
-            temp.pop_back();
-        }
-        
-        if(row <n && col+1<n && maze[row][col+1]==1 && visited[row][col+1] !=1){
-            temp += 'R';
-            solve(maze,ans,temp,n,row,col+1,visited);
-            temp.pop_back();
-        }
-        
-        if(row-1 >=0 && col<n && maze[row-1][col]==1 && visited[row-1][col] !=1){
-            temp += 'U';
-            solve(maze,ans,temp,n,row-1,col,visited);
-            temp.pop_back();
-        }
-        
-        visited[row][col] =0;
     }
   public:
     vector<string> ratInMaze(vector<vector<int>>& maze) {
+        // code here
         vector<string>ans;
-        if(maze[0][0]==0) return ans;
-        
-        int n = maze.size();
-        vector<vector<int>> visited(n,vector<int>(n,0));
-        string temp = "";
-        int row =0;
-        int col =0;
-        
-        solve(maze,ans,temp,n,row,col,visited);
-        
+
+        int nRow = maze.size();
+        int nCol = maze[0].size();
+        string temp;
+
+        solve(maze,ans,temp,0,0,nRow,nCol);
+        sort(ans.begin(),ans.end());
         return ans;
-        
     }
 };
