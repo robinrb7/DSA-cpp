2bdf2e1e1583c9443d5e35b50a4f489ac1aa508f
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-17 03:51:58 +0530

    Time: 373 ms (51.48%), Space: 8.7 MB (87.72%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,36 +1,45 @@
 class Solution {
     private:
-    bool isSafe(vector<vector<char>>& board, int row, int col, char val,int n){
-        for(int i =0;i<n;i++){
-            if(board[i][col]== val) return false;
-
-            if(board[row][i]== val) return false;
-
-            if(board[3*(row/3) + (i/3)][3*(col/3) + (i%3)]== val) return false;
+    bool isPossible(vector<vector<char>>& board,int row,int col,char ch){
+        //for row
+        for(int i =0;i<9;i++){
+            if(board[row][i]==ch) return false;
+        }
+        //for col
+        for(int i =0;i<9;i++){
+            if(board[i][col]==ch) return false;
+        }
+        //for 3X3 box
+        for(int i=0;i<9;i++){
+            int rowIndex = (3 * (row/3)) + i/3;
+            int colIndex = (3 * (col/3)) + i%3;
+            if(board[rowIndex][colIndex]==ch) return false;
         }
 
         return true;
     }
     bool solve(vector<vector<char>>& board){
-        int n= board.size();
-        for(int row =0;row<n;row++){
-            for(int col =0;col<n;col++){
-                if(board[row][col]=='.'){
-                    for(char val= '1';val<='9';val++){
-                        if(isSafe(board,row,col,val,n)){
-                            board[row][col] = val;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='.'){
+
+                    for(char ch ='1';ch<='9';ch++){
+                        if(isPossible(board,i,j,ch)){
+                            board[i][j]=ch;
                             if(solve(board)) return true;
-                            board[row][col] = '.';
+                            board[i][j]='.';
                         }
                     }
                     return false;
                 }
             }
         }
+
         return true;
     }
 public:
     void solveSudoku(vector<vector<char>>& board) {
         solve(board);
+        return;
     }
 };
