e8970f5b949d28fc685db52b839f96ec8db3a775
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-16 17:13:59 +0530

    Time: 126 ms (15.15%), Space: 140.8 MB (15.3%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,43 +1,35 @@
 class Solution {
-    private:
-    bool palin(string s, int i, int j,vector<vector<int>>&dp){
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int left = i, right =j;
-        while(left<right){
-            if(s[left++]!=s[right--]){
-                dp[i][j]=0;
-                return dp[i][j];
-            }
+    bool isPalindrome(string &s, int startIndex, int endIndex){
+        while(startIndex<endIndex){
+            if(s[startIndex++]!=s[endIndex--]) return false;
         }
-        dp[i][j] = 1;
-        return dp[i][j];
+        return true;
     }
-    void solve(string s, vector<vector<string>>&ans, vector<string> &temp, int partIndx, int n,vector<vector<int>>&dp){
-        if(partIndx == n){
-            ans.push_back(temp);
+    void solve(string &s, vector<string> temp, vector<vector<string>>&res, int startIndex, int n){
+        if(startIndex==n){
+            res.push_back(temp);
             return;
-        } 
+        }
 
-        for(int i=partIndx;i<n;i++){
-            if(palin(s,partIndx,i,dp)){
-                temp.push_back(s.substr(partIndx,i - partIndx +1));
-                solve(s,ans,temp,i+1,n,dp);
-                temp.pop_back();
-            }
+        
+
+        for(int partitionIndex = startIndex;partitionIndex<n;partitionIndex++){
+            if(!isPalindrome(s,startIndex, partitionIndex)) continue;
+
+            temp.push_back(s.substr(startIndex,partitionIndex-startIndex+1));
+            solve(s,temp,res,partitionIndex+1,n);
+            temp.pop_back();
         }
 
     }
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string> temp;
-        int n= s.length();
+        vector<vector<string>> res;
+        int n = s.length();
+        if(n==0) return res;
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-
-        solve(s,ans,temp,0,n,dp);
-        return ans;
+        vector<string>temp;
+        solve(s,temp,res,0,n);
+        return res;
     }
 };
