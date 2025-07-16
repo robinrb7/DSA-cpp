1e5de6d6dff9a46530d09ea96cd27bc9f028ed38
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-16 21:22:18 +0530

    Time: 2 ms (75.45%), Space: 10.1 MB (75.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,39 +1,41 @@
 class Solution {
     private:
-    void solve(int n,vector<string> &board, vector<vector<string>> &ans,int col,vector<int>&leftRow,vector<int>&upDiagonal,vector<int>&lowDiagonal){
-        if(col == n){
-            ans.push_back(board);
-            return;
+    bool isValid(vector<string> &board, int row, int col, int n){
+        int dupRow = row, dupCol = col;
+        while(dupCol>=0){
+            if(board[dupRow][dupCol--]=='Q') return false;
+        }
+        dupRow = row, dupCol = col;
+        while(dupRow>=0 && dupCol>=0){
+            if(board[dupRow--][dupCol--]=='Q') return false;
+        }
+        dupRow = row, dupCol = col;
+        while(dupRow<n && dupCol>=0){
+            if(board[dupRow++][dupCol--]=='Q') return false;
         }
 
-        for(int row =0;row<n;row++){
-            if(leftRow[row] ==0 && upDiagonal[n-1+row-col]==0 && lowDiagonal[row+col]==0){
+        return true;
+    }
+    void solve(int col, vector<vector<string>> &res, int n,vector<string> &board){
+        if(col == n){
+            res.push_back(board);
+            return;
+        } 
+
+        for(int row=0;row<n;row++){
+            if(isValid(board,row,col,n)){
                 board[row][col] = 'Q';
-
-                leftRow[row] = 1;
-                upDiagonal[n-1+row-col]=1;
-                lowDiagonal[row+col]=1;
-
-                solve(n,board,ans,col+1,leftRow,upDiagonal,lowDiagonal);
+                solve(col+1,res,n,board);
                 board[row][col] = '.';
-
-                leftRow[row] = 0;
-                upDiagonal[n-1+row-col]=0;
-                lowDiagonal[row+col]=0;
             }
         }
-
     }
 public:
     vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
         vector<string>board(n,string(n,'.'));
-        vector<vector<string>>ans;
-
-        vector<int>leftRow(n,0);
-        vector<int>upDiagonal(2*n-1,0);
-        vector<int>lowDiagonal(2*n-1,0);
-
-        solve(n,board,ans,0,leftRow,upDiagonal,lowDiagonal);
-        return ans;
+        
+        solve(0,res,n,board);
+        return res;
     }
 };
