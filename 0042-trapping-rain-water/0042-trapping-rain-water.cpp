9593e20e0aef19d768a57eda0e797f227bb1564f
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-22 20:45:28 +0530

    Time: 4 ms (11.15%), Space: 27.3 MB (31.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,45 @@
 class Solution {
+    private:
+    void prevGreaterElement(vector<int>& height,vector<int>&prefixMax,int n){
+        prefixMax[0]=height[0];
+        int maxi = prefixMax[0];
+        for(int i=1;i<n;i++){
+            prefixMax[i] = max(height[i],maxi);
+            maxi = prefixMax[i];
+        }
+    }
+
+    void nextGreaterElement(vector<int>& height,vector<int>&suffixMax,int n){
+        suffixMax[n-1] = height[n-1];
+        int maxi = suffixMax[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            suffixMax[i] = max(height[i],maxi);
+            maxi = suffixMax[i];
+        }
+    }
+    
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int l =0, r = n-1;
-        int leftMax = 0,rightMax = 0,total =0;
-    
-        while(l<r){
-            if(height[l]<height[r]){
-                if(leftMax> height[l])
-                    total += leftMax - height[l];
-                leftMax = max(leftMax,height[l]);
-                l++;    
+        vector<int>prefixMax(n,-1);
+        vector<int>suffixMax(n,-1);
+
+        prevGreaterElement(height,prefixMax,n);
+        nextGreaterElement(height,suffixMax,n);
+
+        int width=1;
+        int totalWater = 0;
+        for(int i=0;i<n;i++){
+            int leftMax = prefixMax[i];
+            int rightMax = suffixMax[i];
+
+            if(height[i]< leftMax && height[i]<rightMax){
+                totalWater +=(min(leftMax,rightMax) - height[i]) * width;
             }
-            else{
-                if(rightMax> height[r])
-                    total += rightMax - height[r];
-                rightMax = max(rightMax,height[r]);
-                r--;
-            }
+            
         }
 
-        return total;
+        return totalWater;
     }
 };
