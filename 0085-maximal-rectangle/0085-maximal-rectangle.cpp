e398d7158b902ecff3d7333c47cbc6490e26dded
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-25 06:56:27 +0530

    Time: 7 ms (62.76%), Space: 19.2 MB (46.18%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    int maxRowArea(vector<int>&heights){
+        int n = heights.size();
+        stack<int>st;
+        int maxArea = INT_MIN;
+        for(int i =0;i<n;i++){
+
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                int height = heights[st.top()];
+                st.pop();
+                int width = (st.empty())? i-(-1)-1 : i - st.top() -1;
+                maxArea = max(maxArea,height*width);
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int height = heights[st.top()];
+            st.pop();
+            int width = (st.empty())? n-(-1)-1 : n - st.top() -1;
+            maxArea = max(maxArea,height*width);
+        }
+
+        return maxArea;
+    }
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<vector<int>>prefixSum(n,vector<int>(m,0));
+        for(int j=0;j<m;j++){
+            int sum =0;
+            for(int i=0;i<n;i++){
+                if(matrix[i][j]=='1') sum +=1;
+                else sum = 0;
+                prefixSum[i][j] = sum;
+            }
+        }
+
+        int maxMatrixArea = INT_MIN;
+        for(int i=0;i<n;i++){
+            int area = maxRowArea(prefixSum[i]);
+            maxMatrixArea = max(maxMatrixArea,area);
+        }
+
+        return maxMatrixArea;
+    }
+};
