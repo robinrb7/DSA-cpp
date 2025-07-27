4848b454523f1dab2d80e6d8d72b964548a9a687
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-28 02:39:29 +0530

    Time: 72 ms (79.01%), Space: 55.3 MB (76.85%) - LeetHub

---
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int numSubarrayLooseDistinct(vector<int>& nums, int k){
+        if(k<1) return 0;
+        int n = nums.size();
+        int cnt=0,l=0,r=0;
+        unordered_map<int,int> freq;
+
+        while(r<n){
+            freq[nums[r]]++;
+
+            while(freq.size()>k){
+                freq[nums[l]]--;
+                if(freq[nums[l]]==0) freq.erase(nums[l]);
+                l++;
+            }
+
+            cnt += r-l+1;
+            r++;
+        }
+        return cnt;
+    }
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return (numSubarrayLooseDistinct(nums,k) - numSubarrayLooseDistinct(nums,k-1));
+    }
+};
