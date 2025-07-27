878774ff61542297e0ca6cb42897054a98ccb64c
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-07-27 14:11:42 +0530

    Time: 40 ms (58.48%), Space: 75.2 MB (31.95%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+
+        unordered_map<int,int> fruitType;
+        int l =0, maxLen=0;
+
+        for(int r = 0;r<n;r++){
+            fruitType[fruits[r]]++;
+
+            while(fruitType.size()>2){
+                fruitType[fruits[l]]--;
+                if(fruitType[fruits[l]]==0) fruitType.erase(fruits[l]);
+                l++;
+            }
+
+            maxLen = max(maxLen,r-l+1);
+        }
+        return maxLen;
+    }
+};
