fa1db34f8da8367d5d4fccf1643224d44c3c3c65
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-03 00:23:05 +0530

    Time: 0 ms (100%), Space: 22.1 MB (91.52%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,21 +3,28 @@ public:
     int candy(vector<int>& ratings) {
         int n= ratings.size();
 
-        vector<int>left(n);
+        int totalCandy=1 , i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                totalCandy +=1;
+                i++;
+                continue;
+            }
 
-        left[0]=1;
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]) left[i]=left[i-1] + 1;
-            else left[i]=1;
-        }
+            int peak=1;
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                totalCandy += peak;
+                i++;
+            }
 
-        int totalCandy = max(1,left[n-1]);
-        int right = 1;
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]) right =right + 1;
-            else right=1;
-
-            totalCandy += max(right,left[i]);
+            int down=1;
+            while(i<n && ratings[i] < ratings[i-1]){
+                totalCandy += down;
+                i++;
+                down++;
+            }
+            if(down>peak) totalCandy += (down-peak);
         }
 
         return totalCandy;
