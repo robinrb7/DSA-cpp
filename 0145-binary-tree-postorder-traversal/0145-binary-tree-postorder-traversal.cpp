05849dfc225abd3c45563f380e7d74cac2d1ab7b
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-03 21:50:47 +0530

    Time: 0 ms (100%), Space: 10.9 MB (71.4%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -15,21 +15,29 @@ public:
         vector<int>postorder;
         if(root==NULL) return postorder;
 
-        stack<TreeNode*>st1;
-        stack<int>st2;
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode* top = st1.top();
-            st1.pop();
-            
-            if(top->left) st1.push(top->left);
-            if(top->right) st1.push(top->right);
-
-            st2.push(top->val);
-        }
-        while(!st2.empty()){
-            postorder.push_back(st2.top());
-            st2.pop();
+        stack<TreeNode*>st;
+        TreeNode* node = root;
+        while(node || !st.empty()){
+            if(node){
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                TreeNode* temp = st.top()->right;
+                if(temp){
+                    node=temp;
+                }
+                else{
+                    temp = st.top();
+                    postorder.push_back(temp->val);
+                    st.pop();
+                    while(!st.empty() && temp==st.top()->right){
+                        temp = st.top();
+                        postorder.push_back(temp->val);
+                        st.pop();
+                    }
+                }
+            }
         }
 
         return postorder;
