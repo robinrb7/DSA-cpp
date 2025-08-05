bfe722d98ae6d25cbd9eb13ec383c5e541786056
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-05 07:12:43 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.26%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -15,26 +15,19 @@ public:
         vector<int> res;
         if(!root) return res;
 
-        unordered_map<int,int> uMap;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
-        int mini=0,maxi=0;
+        queue<TreeNode*> q;
+        q.push(root);
         while(!q.empty()){
-            pair<TreeNode*,int> qFront = q.front();
-            q.pop();
-            TreeNode* node = qFront.first;
-            int level = qFront.second;
-            uMap[level]= node->val;
+            res.push_back(q.back()->val);
+            int size = q.size();
 
-            mini=min(mini,level);
-            maxi=max(maxi,level);
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
 
-            if(node->left) q.push({node->left,level+1});
-            if(node->right) q.push({node->right,level+1});
-        }
-
-        for(int i=mini;i<=maxi;i++){
-            res.push_back(uMap[i]);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
         }
 
         return res;
