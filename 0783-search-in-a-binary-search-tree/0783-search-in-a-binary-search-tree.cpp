0b193bf3dc52509bab120ca91febf8aabc4c29a7
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-07 21:24:29 +0530

    Time: 0 ms (100%), Space: 35.5 MB (60.93%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -14,8 +14,11 @@ public:
     TreeNode* searchBST(TreeNode* root, int val) {
         if(!root) return NULL;
 
-        if(root->val == val) return root;
-        else if(root->val > val) return searchBST(root->left,val);
-        else return searchBST(root->right,val);
+        while(root){
+            if(root->val == val) return root;
+            else if(root->val > val)  root=root->left;
+            else  root=root->right;
+        }
+        return root;
     }
 };
