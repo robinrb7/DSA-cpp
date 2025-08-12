d070f445057f576621cc731aeee5b530e7286783
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-13 00:10:54 +0530

    Time: 0 ms (100%), Space: 20 MB (52.75%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -8,6 +8,22 @@ class Solution {
             }
         }
     }
+
+    void bfs(int node,vector<int>& visited,vector<vector<int>>& isConnected){
+        visited[node]=1;
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            int top = q.front();
+            q.pop();
+            for(int i = 0;i<isConnected.size();i++){
+                if(isConnected[top][i]==1 && visited[i]==0){
+                    q.push(i);
+                    visited[i]=1;
+                }
+            }
+        }
+    }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
@@ -17,7 +33,7 @@ public:
         for(int i=0;i<visited.size();i++){
             if(visited[i]==0){
                 provinceCount++;
-                dfs(i,visited,isConnected);
+                bfs(i,visited,isConnected);
             }
         }
 
