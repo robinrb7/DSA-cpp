29b8abf1f6dbbd6c1b85193dc5bef2651681ce42
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-14 03:07:30 +0530

    Time: 811 ms (13.83%), Space: 39.2 MB (11.52%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(beginWord==endWord) return 0;
+
+        unordered_set<string> s;
+        bool foundEndWord=0;
+        for(auto it: wordList){
+            if(it==endWord) foundEndWord=1;
+            s.insert(it);
+        }
+        if(!foundEndWord) return 0;
+
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        while(!q.empty()){
+            auto[word,depth] = q.front();
+            q.pop();
+            if(word==endWord) return depth;
+            s.erase(word);
+
+            for(int i=0;i<word.length();i++){
+                string temp = word;
+                for(char ch='a';ch<='z';ch++){
+                    if(ch==temp[i]) continue;
+                    temp[i] = ch;
+                    if(s.find(temp) != s.end()) q.push({temp,depth+1});
+                }
+            }
+        }
+
+        return 0;
+    }
+};
