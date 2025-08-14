0a464dcb09b58627713fe4306fc51634c8c68010
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-14 06:44:54 +0530

    Time: 0 ms (100%), Space: 17.5 MB (38.05%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -13,6 +13,27 @@ class Solution {
         }
         return true;
     }
+
+    bool bfs(vector<vector<int>>& graph,vector<int> &color, int startNode, int nodeColor){
+
+        queue<pair<int,int>>q;
+        q.push({startNode,nodeColor});
+        while(!q.empty()){
+            auto[node,colValue] = q.front();
+            q.pop();
+
+            for(auto neigh : graph[node]){
+                if(color[neigh]==-1){
+                    q.push({neigh,!colValue});
+                    color[neigh] = !colValue;
+                }
+                else if(color[neigh]==colValue) return false;
+            }
+        }
+
+        return true;
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int v = graph.size();
@@ -20,7 +41,7 @@ public:
         vector<int> color(v,-1);
         for(int i=0;i<v;i++){
             if(color[i]==-1){
-                if(!dfs(graph,color,i,0)) return false;
+                if(!bfs(graph,color,i,0)) return false;
             }
         }
 
