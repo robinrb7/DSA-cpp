7708ac4605ef203ce02b4d20536666e0e0abe06c
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-15 00:45:03 +0530

    Time: 10 ms (25.7%), Space: 19.2 MB (86.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,11 +1,11 @@
 class Solution {
-    bool checkCycle(vector<vector<int>> &adjList,vector<int> &visited,vector<int> &pathVis, int node){
+    bool checkCycleDFS(vector<vector<int>> &adjList,vector<int> &visited,vector<int> &pathVis, int node){
         visited[node]=1;
         pathVis[node]=1;
 
         for(auto neigh : adjList[node]){
             if(visited[neigh]==0){
-                if(checkCycle(adjList,visited,pathVis,neigh)) return true;
+                if(checkCycleDFS(adjList,visited,pathVis,neigh)) return true;
             }
             else if(pathVis[neigh]==1) return true;
         }
@@ -13,20 +13,50 @@ class Solution {
         pathVis[node]=0;
         return false;
     }
+
+    bool checkCycleBFS(vector<vector<int>> &adjList,vector<int> &visited,vector<int>& indegree, int startNode){
+        queue<int>q;
+        for(int i=startNode;i<visited.size();i++){
+            if(indegree[i]==0){
+                q.push(i);
+                visited[i]=1;
+            } 
+        }
+
+        int cnt=0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+
+            for(auto neigh: adjList[node]){
+                indegree[neigh]--;
+                if(indegree[neigh]==0){
+                    q.push(neigh);
+                    visited[neigh]=1;
+                }
+            }
+        }
+
+        if(cnt!=visited.size()) return true;
+        return false;
+    }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
+
+        vector<int>visited(n,0);
+        vector<int> indegree(n,0);
+
         vector<vector<int>> adjList(n);
         for(int i=0;i<prerequisites.size();i++){
             adjList[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
         }
-
-        vector<int>visited(n,0);
-        vector<int> pathVis(n,0);
 
         for(int i=0;i<n;i++){
             if(visited[i]==0){
-                if(checkCycle(adjList,visited,pathVis,i)) return false;
+                if(checkCycleBFS(adjList,visited,indegree, i)) return false;
             }
         }
 
