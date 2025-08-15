0582ec2ac456a78576d3677e137b6f6c0ea8acfe
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-16 02:39:08 +0530

    Time: 51 ms (43.34%), Space: 67.5 MB (8.3%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -19,15 +19,47 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
 
-        vector<int>visited(n,0);
-        vector<int> safeNode(n,0);
-        vector<int> pathVisited(n,0);
-
+        vector<vector<int>>adjList(n);
         for(int i=0;i<n;i++){
-            if(visited[i]==0){
-                dfs(graph,visited,safeNode,pathVisited,i);
+            for(auto neigh: graph[i]){
+                adjList[neigh].push_back(i);
             }
         }
+
+        vector<int>visited(n,0);
+        vector<int> safeNode(n,0);
+        vector<int> indegree(n,0);
+
+        for(int i=0;i<n;i++){
+            for(auto neigh : adjList[i]){
+                indegree[neigh]++;
+            }
+        }
+
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+                visited[i]=1;
+            } 
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safeNode[node]=1;
+
+            for(auto neigh: adjList[node]){
+                if(visited[neigh]==0){
+                    indegree[neigh]--;
+                    if(indegree[neigh]==0){
+                        q.push(neigh);
+                        visited[neigh]=1;
+                    }
+                }
+            }
+        }
+        
 
         vector<int> res;
         for(int i=0;i<n;i++){
