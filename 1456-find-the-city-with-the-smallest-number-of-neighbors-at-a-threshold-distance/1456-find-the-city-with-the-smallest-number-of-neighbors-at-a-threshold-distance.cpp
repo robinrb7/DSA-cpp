8ceca6dab6716af1943cc4c1881ff7fc49d2a63a
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-20 04:57:58 +0530

    Time: 19 ms (37.86%), Space: 20.4 MB (18.4%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,45 +1,61 @@
 class Solution {
+    void dijkstra(vector<vector<pair<int,int>> > &adjList,int startNode,int dThreshold,int &minCityNo,int &minCityCounter,int n){
+        vector<int> dist(n,INT_MAX);
+        priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > minHeap;
+        minHeap.push({0,startNode});
+        dist[startNode]=0;
+
+        while(!minHeap.empty()){
+            auto[distance , node] = minHeap.top();
+            minHeap.pop();
+
+            if(distance > dist[node]) continue;
+
+            for(auto it : adjList[node]){
+                int neigh = it.first;
+                int edgeDist = it.second;
+
+                if(distance + edgeDist < dist[neigh]){
+                    dist[neigh] = distance + edgeDist;
+                    minHeap.push({dist[neigh] , neigh});
+                }
+            }
+        }
+
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(dist[i]!= INT_MAX && dist[i]<= dThreshold) cnt++;
+        }
+
+        if(cnt < minCityCounter){
+            minCityCounter = cnt;
+            minCityNo = startNode;
+        }
+        else if(cnt==minCityCounter) minCityNo = startNode;
+
+        return;
+    }
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         
-        vector<vector<int>> distMatrix(n,vector<int>(n,INT_MAX));
+        vector< vector<pair<int,int>> >adjList(n);
 
         for(int i=0;i<edges.size();i++){
             int u = edges[i][0];
             int v = edges[i][1];
             int dist = edges[i][2];
 
-            distMatrix[u][v] = dist;
-            distMatrix[v][u] = dist;
+            adjList[u].push_back({v,dist});
+            adjList[v].push_back({u,dist});
         }
 
+        int minCityNo = -1;
+        int minCityCounter = INT_MAX;
         for(int i=0;i<n;i++){
-            distMatrix[i][i]=0;
+            dijkstra(adjList,i,distanceThreshold,minCityNo,minCityCounter,n);
         }
 
-        for(int k=0;k <n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(distMatrix[i][k] != INT_MAX && distMatrix[k][j] != INT_MAX)
-                        distMatrix[i][j] = min(distMatrix[i][j], distMatrix[i][k] + distMatrix[k][j]);
-                }
-            }
-        }
-
-        int mini = INT_MAX;
-        int minCity = -1;
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(distMatrix[i][j] != INT_MAX && distMatrix[i][j] <=distanceThreshold) cnt++;
-            }
-            if(cnt < mini){
-                mini = cnt;
-                minCity = i;
-            } 
-            else if(mini == cnt) minCity = i;
-        }
-        return minCity;
+        return minCityNo;
     }
 
 };
