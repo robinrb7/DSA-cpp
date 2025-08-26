7f34cbcbd1c67cdbdbb62a852c9f0510d8e98fad
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-26 12:24:55 +0530

    Time: 0 ms (100%), Space: 10.1 MB (80.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,25 @@
 class Solution {
 private:
-    int solveTab(vector<int>& nums){
+    int solveMemOpt(vector<int>& nums){
         int n =nums.size();
-        vector<int>dp(n,0);
-        dp[0]=nums[0];
+        int prev1=nums[0];
+        int prev2=0;
+        int curr = -1;
 
         for(int index=1;index<n;index++){
             int pick = nums[index];
-            if(index>=2) pick += dp[index-2];
-            int notPick = 0 + dp[index-1];
+            if(index>=2) pick += prev2;
+            int notPick = 0 + prev1;
 
-            dp[index] =  max(pick,notPick);
+           curr =  max(pick,notPick);
+           prev2=prev1;
+           prev1=curr;
         }
         
-        return dp[n-1];
+        return prev1;
     }
 public:
     int rob(vector<int>& nums) {
-        return solveTab(nums);
+        return solveMemOpt(nums);
     }
 };
