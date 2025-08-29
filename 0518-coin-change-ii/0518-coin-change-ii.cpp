0c91f43ea02b28815417c31a04c0167b8fd5ced6
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-29 20:59:03 +0530

    Time: 16 ms (65.39%), Space: 28.5 MB (46.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
     int solve(vector<int> &coins,int amount,int index,vector<vector<int>> &dp){
-        if(index==coins.size() || amount<0) return 0;
+        if(index<0 || amount<0) return 0;
         if(amount==0) return 1;
         if(dp[index][amount] != -1) return dp[index][amount];
 
         int include = solve(coins,amount-coins[index],index,dp);
-        int exclude = solve(coins,amount,index+1,dp);
+        int exclude = solve(coins,amount,index-1,dp);
 
         return dp[index][amount] = include + exclude;
     }
+
+    
 public:
     int change(int amount, vector<int>& coins) {
         int n= coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(coins,amount,0,dp);
+       vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        return solve(coins,amount,n-1,dp);
     }
 };
