120a84596aeac1e4614ae2e3b3f548e98934d8df
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-29 15:14:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,19 @@
 class Solution {
+    int solve(int rowIndex,int colIndex,vector<vector<int>> &dp){
+        if(rowIndex<0 || colIndex<0) return 0;
+        if(rowIndex==0 && colIndex==0) return 1;
+
+        if(dp[rowIndex][colIndex]!= -1) return dp[rowIndex][colIndex];
+
+        int up = solve(rowIndex-1,colIndex,dp);
+        int left = solve(rowIndex,colIndex-1,dp);
+
+        dp[rowIndex][colIndex]= up + left;
+        return dp[rowIndex][colIndex];
+    }
 public:
     int uniquePaths(int m, int n) {
-        int N = m+n-2;
-        int r = min(m-1,n-1);
-        long long ans = 1;
-
-        for(int i =0;i<r;i++){
-            ans = ans* (N-i)/(i+1);
-        }
-
-        return (int)ans;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
     }
 };
