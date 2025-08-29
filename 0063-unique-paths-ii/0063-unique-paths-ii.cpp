649292cc7888271631309ac93aefeb736f1f8016
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-08-29 16:52:28 +0530

    Time: 0 ms (100%), Space: 11.9 MB (55.12%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,18 +5,36 @@ private:
         if(rowIndex==0 && colIndex==0) return 1;
 
         if(dp[rowIndex][colIndex] != -1) return dp[rowIndex][colIndex];
-        
-        int up = solve(rowIndex-1,colIndex,obstacleGrid,dp);
-        int down = solve(rowIndex,colIndex-1,obstacleGrid,dp);
 
-        dp[rowIndex][colIndex] =  up + down;
+        int up = solve(rowIndex-1,colIndex,obstacleGrid,dp);
+        int left = solve(rowIndex,colIndex-1,obstacleGrid,dp);
+
+        dp[rowIndex][colIndex] =  up + left;
         return dp[rowIndex][colIndex];
+    }
+
+    int solveTab( int n, int m,vector<vector<int>>& obstacleGrid){
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        dp[0][0]=1;
+
+        for(int rowIndex=0;rowIndex<n;rowIndex++){
+            for(int colIndex=0;colIndex<m;colIndex++){
+                if(rowIndex==0 && colIndex==0) continue;
+                if(obstacleGrid[rowIndex][colIndex]==1) dp[rowIndex][colIndex]=0;
+                else{
+                    int up=0,left=0;
+                    if(rowIndex>0) up = dp[rowIndex-1][colIndex];
+                    if(colIndex>0) left = dp[rowIndex][colIndex-1];
+                    dp[rowIndex][colIndex] =  up + left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid[0][0]==1) return 0;
         int n=obstacleGrid.size(), m = obstacleGrid[0].size();
-
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,obstacleGrid,dp);
+        return solveTab(n,m,obstacleGrid);
     }
 };
