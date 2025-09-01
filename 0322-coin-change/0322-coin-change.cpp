306dd89ba478d652042d26596f505b35bead3f63
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-01 21:37:49 +0530

    Time: 49 ms (29.43%), Space: 49.4 MB (31.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,27 @@
 class Solution {
 private:
-    
-    int solve(vector<int>& coins, int amount){
-        vector<int> dp(amount+1,0);
-        dp[0]=0;
-
-        for(int price=1;price<amount+1;price++){
-            int minCoins = INT_MAX;
-            for(int i=0;i<coins.size();i++){
-                int coinsReq = INT_MAX;
-                if(price-coins[i] >=0){
-                    if(dp[price-coins[i]] !=-1){
-                        coinsReq = 1 + dp[price-coins[i]];
-                        minCoins = min(minCoins,coinsReq);
-                    } 
-                }   
-            }
-            dp[price] = (minCoins==INT_MAX)? -1: minCoins;
+    int solve(vector<int>& coins,int index, int amount,vector<vector<int>> &dp){
+        if(amount==0) return 0;
+        if(index==0){
+            if(amount%coins[0]==0) return amount/coins[0];
+            else return 1e9;
         }
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        
+        int notTake = 0 + solve(coins,index-1,amount,dp);
+        int take = INT_MAX;
+        if(amount >= coins[index]) take = 1 + solve(coins,index,amount-coins[index],dp);
+        
 
-        return dp[amount];
+        return dp[index][amount] = min(take,notTake);
     }
 
 public:
     int coinChange(vector<int>& coins, int amount) {
-        
-        return  solve(coins,amount);
+        int n = coins.size();
+
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans =  solve(coins,n-1,amount,dp);
+        return (ans>=1e9)? -1: ans;
     }
 };
