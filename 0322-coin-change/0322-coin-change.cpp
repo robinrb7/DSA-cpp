7cd4e134ae3c9c746bf2eb8b1c165118c8f09bfe
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-01 21:53:47 +0530

    Time: 49 ms (29.43%), Space: 48.9 MB (43.97%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -16,12 +16,34 @@ private:
         return dp[index][amount] = min(take,notTake);
     }
 
+    int solveTab(vector<int>& coins, int cost){
+        int n =coins.size();
+        vector<vector<int>> dp(n,vector<int>(cost+1,0));
+        for(int i=0;i<=cost;i++){
+            if(i%coins[0]==0) dp[0][i]= i/coins[0];
+            else dp[0][i] = 1e9;
+        }
+
+        for(int index=1;index<n;index++){
+            for(int amount=1;amount<=cost;amount++){
+
+                int notTake = 0 + dp[index-1][amount];
+                int take = INT_MAX;
+                if(amount >= coins[index]) take = 1 + dp[index][amount-coins[index]];
+                
+                dp[index][amount] = min(take,notTake);
+            }
+        }
+        
+        return dp[n-1][cost];
+    }
+
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans =  solve(coins,n-1,amount,dp);
+        
+        int ans =  solveTab(coins,amount);
         return (ans>=1e9)? -1: ans;
     }
 };
