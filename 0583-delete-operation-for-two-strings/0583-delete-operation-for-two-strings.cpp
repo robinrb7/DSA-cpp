594ae86b9fc2a5f7b2d13effc8e5801cbfb794e7
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-03 17:47:48 +0530

    Time: 15 ms (34.16%), Space: 17.3 MB (14.1%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -14,11 +14,32 @@ class Solution {
         }
         return dp[index1][index2] = ans;
     }
+
+    int solveTab(string word1,string word2){
+        int n1=word1.length(), n2=word2.length();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+
+        for(int j=0;j<=n2;j++) dp[0][j] = j;
+        for(int i=0;i<=n1;i++) dp[i][0] = i;
+
+        for(int index1=1;index1<=n1;index1++){
+            for(int index2=1;index2<=n2;index2++){
+                int ans=0;
+                if(word1[index1-1]==word2[index2-1]){
+                    ans = dp[index1-1][index2-1];
+                }
+                else{
+                    ans = 1 + min(dp[index1-1][index2],dp[index1][index2-1]);
+                }
+                dp[index1][index2] = ans;
+            }
+        }
+        return dp[n1][n2];        
+    }
 public:
     int minDistance(string word1, string word2) {
         int n1=word1.length(), n2=word2.length();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
 
-        return solve(word1,word2,n1-1,n2-1,dp);
+        return solveTab(word1,word2);
     }
 };
