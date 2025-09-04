0393a3596f4227413c004f7250a0e2db815c8af8
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-04 17:06:04 +0530

    Time: 68 ms (7.63%), Space: 81.5 MB (12.66%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -6,8 +6,7 @@ class Solution {
         
         if(index1 <0) return index2+1;
         if(index2 <0) return index1+1;
-
-        if(dp[index1+1][index2+1] != -1) return dp[index1+1][index2+1];
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
 
         int ans = 0;
         if(word1[index1]==word2[index2]){
@@ -21,39 +20,7 @@ class Solution {
             ans = min(insertCh,min(deleteCh,replaceCh));
         }
 
-        return dp[index1+1][index2+1] = ans;
-    }
-
-    int solveTab(string word1,string word2){
-        int n1 = word1.length();
-        int n2 = word2.length();
-
-        vector<int> prev(n2+1,0);
-        vector<int> curr(n2+1,0);
-        prev[0]=0;
-        for(int j=0;j<=n2;j++) prev[j] = j;
-
-        for(int index1=1;index1<=n1;index1++){
-            curr[0]=index1;
-            for(int index2=1;index2<=n2;index2++){
-                int ans = 0;
-                if(word1[index1-1]==word2[index2-1]){
-                    ans = 0 + prev[index2-1];
-                }
-                else{
-                    int insertCh = 1 + curr[index2-1];
-                    int deleteCh = 1 + prev[index2];
-                    int replaceCh = 1 + prev[index2-1];
-
-                    ans = min(insertCh,min(deleteCh,replaceCh));
-                }
-
-                curr[index2] = ans;
-            }
-            prev=curr;
-        }
-
-        return prev[n2];
+        return dp[index1][index2] = ans;
     }
 
 
@@ -62,10 +29,7 @@ public:
         int n1 = word1.length();
         int n2 = word2.length();
 
-        if(n1==0 && n2==0) return 0;
-        if(n1==0 && n2!=0) return n2;
-        if(n1!=0 && n2==0) return n1;
-
-        return solveTab(word1,word2);
+        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        return solve(word1,word2,n1-1,n2-1,dp);
     }
 };
