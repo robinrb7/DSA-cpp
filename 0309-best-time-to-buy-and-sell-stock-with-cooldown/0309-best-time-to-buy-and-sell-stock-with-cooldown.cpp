b2752355bd16cf5d65c2752fab25794a983c5226
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-05 18:10:21 +0530

    Time: 0 ms (100%), Space: 14.9 MB (95.07%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -18,14 +18,17 @@ private:
 
 int solveTab(vector<int>& prices){
     int n=prices.size();
-    vector<vector<int>> dp(n+2,vector<int>(3,0)); 
+    vector<int>front1(2,0),front2(2,0) , curr(2,0); 
 
     for(int index=n-1;index>=0;index--){
-        dp[index][1] = max(-prices[index] + dp[index+1][0] , dp[index+1][1]);
-        dp[index][0] = max(prices[index] + dp[index+2][1] ,dp[index+1][0]);
+        curr[1] = max(-prices[index] + front1[0] , front1[1]);
+        curr[0] = max(prices[index] + front2[1] , front1[0]);
+
+        front2=front1;
+        front1=curr;
     }
 
-    return dp[0][1];
+    return front1[1];
 }
 public:
     int maxProfit(vector<int>& prices) {
