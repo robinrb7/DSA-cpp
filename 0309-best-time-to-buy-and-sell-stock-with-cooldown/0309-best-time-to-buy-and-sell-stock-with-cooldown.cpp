ec696c82fca253585fc1757128b037400087c733
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-05 17:54:09 +0530

    Time: 0 ms (100%), Space: 16.1 MB (64.5%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -15,11 +15,22 @@ private:
 
     return dp[index][buyAllowed] = ans;
 }
+
+int solveTab(vector<int>& prices){
+    int n=prices.size();
+    vector<vector<int>> dp(n+2,vector<int>(3,0)); 
+
+    for(int index=n-1;index>=0;index--){
+        dp[index][1] = max(-prices[index] + dp[index+1][0] , dp[index+1][1]);
+        dp[index][0] = max(prices[index] + dp[index+2][1] ,dp[index+1][0]);
+    }
+
+    return dp[0][1];
+}
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(prices,0,1,dp);
+        return solveTab(prices);
     }
 };
