a17fc78dd524723448e0d4d6ece07fdf5adb267f
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-05 14:26:37 +0530

    Time: 11 ms (5.09%), Space: 21.8 MB (20.85%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -15,11 +15,32 @@ int solve(vector<int>& prices, int index,int buyAllowed,vector<vector<int>> &dp)
 
     return dp[index][buyAllowed] = ans;
 }
+
+int solveTab(vector<int>& prices){
+    int n=prices.size();
+    vector<vector<int>> dp(n+1,vector<int>(2,0));
+
+    for(int index=n-1;index>=0;index--){
+        for(int buyAllowed=0;buyAllowed<2;buyAllowed++){
+            int ans = 0;
+            if(buyAllowed){
+                ans = max(-prices[index] + dp[index+1][0] , dp[index+1][1]);
+            }
+            else{
+                ans = max(prices[index] + dp[index+1][1] , dp[index+1][0]);
+            }
+
+            dp[index][buyAllowed] = ans;
+        }
+    }
+    
+    return dp[0][1];
+}
 public:
     int maxProfit(vector<int>& prices) {
         int n =prices.size();
 
         vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        return solve(prices,0,1,dp);
+        return solveTab(prices);
     }
 };
