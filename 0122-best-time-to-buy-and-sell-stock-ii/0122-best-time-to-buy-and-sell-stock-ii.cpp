83cd6fb7d94c800b31e216db258fdb89f1f6975e
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-05 14:53:45 +0530

    Time: 0 ms (100%), Space: 17.2 MB (48.93%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,29 +18,24 @@ int solve(vector<int>& prices, int index,int buyAllowed,vector<vector<int>> &dp)
 
 int solveTab(vector<int>& prices){
     int n=prices.size();
-    vector<vector<int>> dp(n+1,vector<int>(2,0));
+    int frontBuy=0, frontSell=0;
 
     for(int index=n-1;index>=0;index--){
-        for(int buyAllowed=0;buyAllowed<2;buyAllowed++){
-            int ans = 0;
-            if(buyAllowed){
-                ans = max(-prices[index] + dp[index+1][0] , dp[index+1][1]);
-            }
-            else{
-                ans = max(prices[index] + dp[index+1][1] , dp[index+1][0]);
-            }
+        int currBuy=0, currSell=0;
 
-            dp[index][buyAllowed] = ans;
-        }
+        currBuy = max(-prices[index] + frontSell , frontBuy);
+        currSell = max(prices[index] + frontBuy , frontSell);
+
+        frontBuy = currBuy;
+        frontSell = currSell;
     }
     
-    return dp[0][1];
+    return frontBuy;
 }
 public:
     int maxProfit(vector<int>& prices) {
         int n =prices.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
         return solveTab(prices);
     }
 };
