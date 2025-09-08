df4abd61679017c9de732ce04112e602be5e39d2
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-08 17:32:37 +0530

    Time: 18 ms (84.27%), Space: 13.9 MB (78.36%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -11,13 +11,34 @@ private:
         }
         return dp[i][j] = mini;
     }
+
+    int solveTab(vector<int> &cuts){
+        int N = cuts.size();
+        vector<vector<int>> dp(N,vector<int>(N,0));
+
+        for(int i =N-2;i>=1;i--){
+            for(int j=i;j<=N-2;j++){
+
+                int mini=INT_MAX;
+                for(int index=i;index<=j;index++){
+                    int cost = cuts[j+1]-cuts[i-1] + dp[i][index-1] + dp[index+1][j];
+                    mini = min(mini,cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
+
+        return dp[1][N-2];
+    }
+
+
 public:
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0), cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
         int N = cuts.size();
 
-        vector<vector<int>> dp(N,vector<int>(N,-1));
-        return solve(cuts,1,N-2,dp);
+        
+        return solveTab(cuts);
     }
 };
