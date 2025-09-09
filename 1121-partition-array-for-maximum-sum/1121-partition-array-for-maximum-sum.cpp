9678296a6cd9825cc18ffc940709abec4aa532f4
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-10 01:19:59 +0530

    Time: 7 ms (52.39%), Space: 12.6 MB (9.24%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -17,11 +17,30 @@ private:
 
         return dp[i] = maxi;
     }
+
+    int solveTab(vector<int> &arr,int k){
+        int n = arr.size();
+        vector<int> dp(n+1,0);
+
+        for(int i=n-1;i>=0;i--){
+            int maxi =INT_MIN;
+            int len=0, maxElement=INT_MIN;
+            for(int index=i;index<min(n,i+k);index++){
+                len++;
+                maxElement = max(maxElement,arr[index]);
+                int sum = (len*maxElement) + dp[index+1];
+                maxi = max(maxi,sum);
+            }
+
+            dp[i] = maxi;
+        }
+        return dp[0];
+    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k){
         int n =arr.size();
 
-        vector<int> dp(n,-1);
-        return solve(arr,k,0,dp);
+        
+        return solveTab(arr,k);
     }
 };
