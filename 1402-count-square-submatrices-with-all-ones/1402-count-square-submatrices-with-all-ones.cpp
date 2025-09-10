0e800fd269ff83c0d87c72352fe680200b5fbbb8
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-10 15:16:50 +0530

    Time: 0 ms (100%), Space: 27.5 MB (74.86%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -3,30 +3,29 @@ public:
     int countSquares(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<int> prev(m,0) ,curr(m,0);
+
         int cnt = 0;
-        for(int i=0;i<n;i++){
-            dp[i][0] = matrix[i][0];
-            cnt += dp[i][0];
-        } 
-        for(int j=1;j<m;j++){
-            dp[0][j] = matrix[0][j];
-            cnt += dp[0][j];
+        for(int j=0;j<m;j++){
+            prev[j] = matrix[0][j];
+            cnt += prev[j];
         } 
 
         for(int i=1;i<n;i++){
+            curr[0] = matrix[i][0];
+            cnt += curr[0];
             for(int j=1;j<m;j++){
-
-                if(matrix[i][j]==0) dp[i][j]=0;
+                if(matrix[i][j]==0) curr[j]=0;
                 else{
-                    int up = dp[i-1][j];
-                    int left = dp[i][j-1];
-                    int diagonal = dp[i-1][j-1];
+                    int up = prev[j];
+                    int left = curr[j-1];
+                    int diagonal = prev[j-1];
 
-                    dp[i][j] = 1 + min(up,min(left,diagonal));
+                    curr[j] = 1 + min(up,min(left,diagonal));
                 }
-                cnt += dp[i][j];
+                cnt += curr[j];
             }
+            prev = curr;
         }
         return cnt;
     }
