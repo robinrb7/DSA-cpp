1d2198a88018f8d5f2651837a51f408c28f4d960
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-12 19:27:29 +0530

    Time: 0 ms (100%), Space: 26 MB (99.63%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -13,7 +13,7 @@ public:
 
             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
 
-            if( (mid%2==0 && nums[mid+1]==nums[mid]) || mid&2!=0 && nums[mid-1] == nums[mid]){
+            if((mid%2==0 && nums[mid+1]==nums[mid]) || mid&2!=0 && nums[mid-1] == nums[mid]){
                 low = mid+1;
             }
             
