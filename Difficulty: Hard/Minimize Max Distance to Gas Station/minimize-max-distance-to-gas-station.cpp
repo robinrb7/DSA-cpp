81f482924d717638f715acc2461f33061a05bc0a
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-15 12:29:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -1,35 +1,34 @@
 class Solution {
-    private:
-    int func(vector<int>&stations, long double dist){
-        int n = stations.size();
-        int cnt = 0;
-        for(int i = 0;i<n-1;i++){
-            int stPlaced = (stations[i+1]-stations[i])/dist;
-            if((stations[i+1]-stations[i])/dist== stPlaced * dist) stPlaced--;
-            
-            cnt += stPlaced;
+private:
+    bool isPossible(vector<int>&stations, int K,double mid){
+        int partitions=0, n= stations.size();
+        for(int i=0;i<n-1;i++){
+            double dist = stations[i+1]-stations[i];
+            if(dist>mid){
+                partitions += (int)(dist/mid);
+            }
+            if(partitions>K) return 0;
         }
-        return cnt;
+        return 1;
     }
   public:
-    double findSmallestMaxDist(vector<int> &stations, int k) {
-        int n= stations.size();
-        
-        long double maxi = 0;
-        for(int i =0;i<n-1;i++){
-            long double diff = stations[i+1] - stations[i];
-            if(diff > maxi) maxi = diff;
+    double minMaxDist(vector<int> &stations, int K) {
+        int n =  stations.size();
+        int maxDistance = INT_MIN;
+        for(int i=0;i<n-1;i++){
+            if(stations[i+1]-stations[i] > maxDistance) 
+                maxDistance = stations[i+1]-stations[i];
         }
         
-        long double low =0, high = maxi;
-        while(high - low > 1e-6){
-            long double mid = low + (high -low)/2.0;
-            
-            int cnt = func(stations,mid);
-            if(cnt <= k) high = mid;
+        double low = 0, high = maxDistance;
+        while(high-low>1e-6){
+            double mid = low + (high-low)/2.0;
+            if(isPossible(stations,K,mid)){
+                high = mid;
+            }
             else low = mid;
         }
         
-        return high;
+        return low;
     }
 };
