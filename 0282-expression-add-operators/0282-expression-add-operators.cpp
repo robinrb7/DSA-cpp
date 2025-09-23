ee942e1e316a368f5b25878bbc9542118d2517e8
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-23 21:53:19 +0530

    Time: 423 ms (41.99%), Space: 93.2 MB (53.73%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,36 +1,33 @@
 class Solution {
-    private:
-    void solve(string num, int target, string temp, vector<string>&ans, int index,int n,long long prev,long long resultant){
+    void solve(string &num, int target,string temp, vector<string> &res,int index,long long resultant,long long prev){
+        int n =num.size();
         if(index==n){
-            if(resultant==target){
-                ans.push_back(temp);
-            }
+            if(resultant==target) res.push_back(temp);
             return;
         }
 
-        for(int i= index;i<n;i++){
+        for(int i=index;i<n;i++){
+            if(i>index && num[index]=='0') return;
             string str = num.substr(index,i-index+1);
-            long long number = stoll(str);
-            if(i>index && num[index]=='0') break;
+            long long curr_val = stoll(str);
+            
 
             if(index==0){
-                solve(num,target,temp+str,ans,i+1,n,number,number);
+                solve(num,target,temp+str,res,i+1,curr_val,curr_val);
             }
             else{
-                solve(num,target,temp + '+' + str,ans,i+1,n,number,resultant+number);
-                solve(num,target,temp + '-' + str,ans,i+1,n,-number,resultant-number);
-                solve(num,target,temp + '*' + str,ans,i+1,n,prev*number,(resultant - prev)+(prev*number));
+                solve(num,target,temp+ '+' +str,res,i+1,resultant + curr_val,curr_val);
+                solve(num,target,temp+ '-' +str,res,i+1,resultant - curr_val,-curr_val);
+                solve(num,target,temp+ '*' +str,res,i+1,(prev*curr_val) + (resultant-prev),prev*curr_val);
             }
         }
-
+        
     }
 public:
     vector<string> addOperators(string num, int target) {
-        int n = num.length();
-        vector<string> ans;
+        vector<string> res;
         string temp="";
-
-        solve(num,target,temp,ans,0,n,0,0);
-        return ans;
+        solve(num,target,temp,res,0,0,0);
+        return res;
     }
 };
