c9a25f0218fd1255e075d938f4ca6e7bfc187527
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-24 16:42:51 +0530

    Time: 0 ms (100%), Space: 20.5 MB (83.42%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int i =0;i<nums.size();i++){
-            ans = ans^nums[i];
+        int n= nums.size();
+        int result = 0;
+        for(int i=0;i<n;i++){
+            result = result ^ nums[i];
         }
 
-        return ans;
+        return result;
     }
 };
