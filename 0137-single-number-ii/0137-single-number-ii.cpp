0f0ecc573991d11f9b66665ddbf1f1b45d17bd87
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-24 20:39:16 +0530

    Time: 0 ms (100%), Space: 13.3 MB (65.78%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n =nums.size();
-        if(n==1) return nums[0];
+        int n = nums.size();
+        int ones=0, twos=0;
 
-        int res=0;
-        for(int bit=0;bit<32;bit++){
-            int count1 = 0;
-            for(int i=0;i<n;i++){
-                if(nums[i] & (1<<bit)) count1++;
-            }
-            if(count1%3==1){
-                res = res |(1<<bit);
-            }
+        for(int i=0;i<n;i++){
+            ones = (nums[i]^ones) & (~twos);
+            twos = (nums[i]^twos) & (~ones);
         }
 
-        return res;
+        return ones;
     }
 };
