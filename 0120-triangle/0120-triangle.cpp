4508a5e538ebc1c23aefe3d758b8695765d05c2c
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-25 18:56:50 +0530

    Time: 0 ms (100%), Space: 12.6 MB (91.62%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -11,15 +11,14 @@ private:
 
     int solveTab(vector<vector<int>>& triangle){
         int n = triangle.size();
-        vector<int> front(n,0), curr(n,0);
+        vector<int> front(n,0);
 
         for(int j=0;j<n;j++) front[j] = triangle[n-1][j];
 
         for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                curr[j] = triangle[i][j] + min(front[j] , front[j+1]);
+            for(int j=0;j<=i;j++){
+                front[j] = triangle[i][j] + min(front[j] , front[j+1]);
             }
-            front = curr;
         }
         
         return front[0];
