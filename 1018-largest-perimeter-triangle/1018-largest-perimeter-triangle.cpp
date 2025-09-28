c64fc107fa29239ba7a815094556240e81342445
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-09-28 18:21:32 +0530

    Time: 16 ms (12.27%), Space: 25.7 MB (46.12%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -1,19 +1,13 @@
 class Solution {
-private:
-    bool isPossible(int num1,int num2,int num3){
-        if(num1+num2>num3 && num2+num3>num1 && num1+num3>num2) return 1;
-        return 0;
-    }
-    
+
 public:
     int largestPerimeter(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
 
         for(int k=n-1;k>=2;k--){
-            if(isPossible(nums[k],nums[k-1],nums[k-2])) return nums[k] + nums[k-1] + nums[k-2];
+            if(nums[k-2]+nums[k-1] > nums[k]) return nums[k] + nums[k-1] + nums[k-2];
         }
-
         return 0;
     }
 };
