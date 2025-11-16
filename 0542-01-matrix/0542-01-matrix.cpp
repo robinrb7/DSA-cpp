8001e0f906a8995e2515078d94985eeaabaf08dd
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-11-16 15:00:01 +0530

    Time: 33 ms (17.76%), Space: 38.6 MB (45.97%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,5 +1,4 @@
 class Solution {
-    private:
 
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
@@ -7,35 +6,37 @@ public:
         int m = mat[0].size();
 
         vector<vector<int>> visited(n,vector<int>(m,0));
+        queue< pair<pair<int,int>,int> > q;
 
-        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+
                 if(mat[i][j]==0){
-                    visited[i][j]=1;
                     q.push({{i,j},0});
-                }
+                    visited[i][j]=1;
+                } 
+                
             }
         }
 
-        int dRow[4] = {-1,1,0,0};
-        int dCol[4] = {0,0,-1,1};
-        while(!q.empty()){
-            auto p = q.front();
-            int row = p.first.first;
-            int col = p.first.second;
-            int step = p.second;
-            q.pop();
+        int dRow[4] = {0,0,-1,1};
+        int dCol[4] = {-1,1,0,0};
 
-            mat[row][col] = step;
+        while(!q.empty()){
+            auto[cord, dist] = q.front();
+            q.pop();
+            auto[row,col] = cord;
 
             for(int i=0;i<4;i++){
-                int nRow = row+dRow[i];
-                int nCol = col+dCol[i];
+                int nRow = row + dRow[i];
+                int nCol = col + dCol[i];
 
-                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && visited[nRow][nCol]==0){
-                    q.push({{nRow,nCol},step+1});
-                    visited[nRow][nCol]=1;
+                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m 
+                    && mat[nRow][nCol]==1 && !visited[nRow][nCol]){
+
+                        visited[nRow][nCol]=1;
+                        mat[nRow][nCol] = dist+1;
+                        q.push({{nRow,nCol}, dist + 1});
                 }
             }
 
