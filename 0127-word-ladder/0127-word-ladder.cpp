e5313554bacba85c5312e284a8c99361de663771
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-11-16 16:28:37 +0530

    Time: 283 ms (31.4%), Space: 21.3 MB (46.01%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        if(beginWord==endWord) return 0;
+        int n = wordList.size();
 
-        unordered_set<string> s;
-        bool foundEndWord=0;
-        for(auto it: wordList){
-            if(it==endWord) foundEndWord=1;
-            s.insert(it);
-        }
-        if(!foundEndWord) return 0;
+        set<string> st(wordList.begin(),wordList.end());
+        if(st.find(endWord) == st.end()) return 0;
 
-        queue<pair<string,int>>q;
+        queue<pair<string,int>> q;
         q.push({beginWord,1});
-        while(!q.empty()){
-            auto[word,depth] = q.front();
-            q.pop();
-            if(word==endWord) return depth;
+        st.erase(beginWord);
 
-            for(int i=0;i<word.length();i++){
-                string temp = word;
-                for(char ch='a';ch<='z';ch++){
-                    if(ch==temp[i]) continue;
-                    temp[i] = ch;
-                    if(s.find(temp) != s.end()){
-                        q.push({temp,depth+1});
-                        s.erase(temp);
-                    } 
+        while(!q.empty()){
+            auto[str, steps] = q.front();
+            q.pop();
+
+            if(str == endWord) return steps;
+
+            for(int i=0;i<str.length();i++){
+                char original = str[i];
+                for(char ch ='a';ch<='z'; ch++){
+                    if(ch==original) continue;
+                    str[i] = ch;
+
+                    if(st.find(str) != st.end()){
+                        st.erase(str);
+                        q.push({str,steps+1});
+
+                    }
                 }
+                str[i]=original;
+                
             }
         }
 
