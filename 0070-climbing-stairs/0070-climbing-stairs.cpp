a6c03cc2d733ab1206653ee6c834fbc863ac0d94
Robin Singh Khural <rkhural_be22@thapar.edu>
2025-12-05 20:41:01 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,52 @@
 class Solution {
-    private:
-    int solve(int n){
-        int prev1 = 1;
-        int prev2 = 1;
-        int curr =-1;
-        
-        for(int index=2;index<=n;index++){
-            curr = prev1 + prev2;
+private:
+    int solve(int stepNo){
+        if(stepNo < 0) return 0;
+        if(stepNo==0 || stepNo==1) return 1;
 
-            prev2=prev1;
-            prev1=curr;
+        int cntWays = solve(stepNo - 1) + solve(stepNo -2);
+        return cntWays;
+    }
+
+    int solveMem(int stepNo,vector<int> &dp){
+        if(stepNo < 0) return 0;
+        if(stepNo==0 || stepNo==1) return 1;
+
+        if(dp[stepNo] != -1) return dp[stepNo];
+
+        int cntWays = solveMem(stepNo - 1,dp) + solveMem(stepNo - 2,dp);
+        return dp[stepNo] = cntWays;
+    }
+
+    int solveTab(int n){
+        vector<int> dp(n+1,0); 
+        dp[0]=dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
+
+        return dp[n];
+    }
+
+    int solveSpaceOpt(int n){
+        int prev2 =1, prev1=1;
+        int curr = -1;
+
+        for(int i=2;i<=n;i++){
+            curr = prev2 + prev1;
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
         return prev1;
     }
+
 public:
     int climbStairs(int n) {
-        
-        return solve(n);
+        if(n==0 || n==1) return n;
+
+        return solveSpaceOpt(n);
     }
 };
