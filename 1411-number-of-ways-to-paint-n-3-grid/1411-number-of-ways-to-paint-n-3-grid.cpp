a5ec40619bb01b563030a4712a576b11053a0e08
Robin Singh Khural <rkhural_be22@thapar.edu>
2026-01-03 23:56:40 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.67%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,45 +1,20 @@
 class Solution {
-    const int MOD = 1e9 + 7;
-private:
-    long long solve(vector<string> &ways, int n, int prevStrIndex,vector<vector<long long>> &dp){
-        if(n==0) return 1;
-        if(dp[n][prevStrIndex]!=-1) return dp[n][prevStrIndex];
-
-        long long ans = 0;
-        string prevString = ways[prevStrIndex];
-        for(int i=0;i<12;i++){
-            if(i==prevStrIndex) continue;
-
-            string currString = ways[i];
-            bool conflict = false;
-            for(int j=0;j<3;j++){
-                if(prevString[j]==currString[j]){
-                    conflict = true;
-                    break;
-                } 
-            }
-
-            if(!conflict){
-                ans = (ans + solve(ways,n-1,i,dp))  % MOD;
-            } 
-        }
-
-        return dp[n][prevStrIndex] = ans;
-    }
 public:
     int numOfWays(int n) {
-        vector<string> ways = {"RGY","RGR","RYR","RYG",
-                                "GYR","GYG", "GRG","GRY",
-                                "YGR","YGY","YRY","YRG"
-                                };
-
-        vector<vector<long long>> dp(n+1,vector<long long>(12,-1));
+        if(n==0) return 0;
         
-        long long ans=0;
-        for(int i=0;i<12;i++){
-            ans = (ans + solve(ways,n-1,i,dp))  % MOD;
+        const int MOD= 1e9 + 7;
+        long twoPattern = 6, threePattern = 6;
+        n--;
+
+        while(n>0){
+            long newTwoPattern = (3*twoPattern + 2*threePattern) % MOD;
+            threePattern = (2*twoPattern + 2*threePattern) % MOD;
+            twoPattern = newTwoPattern;
+
+            n--;
         }
 
-        return (int)ans;
+        return (twoPattern + threePattern) % MOD;
     }
 };
