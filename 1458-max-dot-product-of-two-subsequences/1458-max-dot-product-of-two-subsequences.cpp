039c0c7cd1ea17a149fbf2a9f8dfc5d60985cc91
Robin Singh Khural <rkhural_be22@thapar.edu>
2026-01-08 21:08:39 +0530

    Time: 19 ms (59.2%), Space: 17.8 MB (54.8%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,20 +1,22 @@
 class Solution {
 private:
-    int solve(vector<int>& nums1, vector<int>& nums2, int index1,int index2){
+    int solve(vector<int>& nums1, vector<int>& nums2, int index1,int index2,vector<vector<int>> &dp){
         if(index1<0 || index2<0) return INT_MIN;
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
 
         int include = (nums1[index1]*nums2[index2]);
 
-        if(index1>0 && index2>0) include += max(0,solve(nums1,nums2,index1-1,index2-1));
-        int exclude1 = 0 + solve(nums1,nums2,index1-1,index2);
-        int exclude2 = 0 + solve(nums1,nums2,index1,index2-1);
+        if(index1>0 && index2>0) include += max(0,solve(nums1,nums2,index1-1,index2-1,dp));
+        int exclude1 = 0 + solve(nums1,nums2,index1-1,index2,dp);
+        int exclude2 = 0 + solve(nums1,nums2,index1,index2-1,dp);
 
-        return max(include,max(exclude1,exclude2));
+        return dp[index1][index2] = max(include,max(exclude1,exclude2));
     }
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m=nums2.size();
         
-        return solve(nums1,nums2,n-1,m-1);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(nums1,nums2,n-1,m-1,dp);
     }
 };
