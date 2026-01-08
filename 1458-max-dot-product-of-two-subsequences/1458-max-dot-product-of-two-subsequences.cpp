bfbab427f0352c98eda2e59a806e39c4ba6b7147
Robin Singh Khural <rkhural_be22@thapar.edu>
2026-01-08 21:39:14 +0530

    Time: 7 ms (96.8%), Space: 14.1 MB (97.6%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -21,15 +21,36 @@ private:
             for(int index2=0;index2<m;index2++){
                 int include = (nums1[index1]*nums2[index2]);
 
-                if(index1>0 && index2>0) include += max(0,solve(nums1,nums2,index1-1,index2-1,dp));
-                int exclude1 = 0 + solve(nums1,nums2,index1-1,index2,dp);
-                int exclude2 = 0 + solve(nums1,nums2,index1,index2-1,dp);
+                if(index1>0 && index2>0) include += max(0,dp[index1-1][index2-1]);
+                int exclude1 = INT_MIN, exclude2 = INT_MIN;
+                if(index1>=1) exclude1 = dp[index1-1][index2];
+                if(index2>=1) exclude2 = dp[index1][index2-1];
 
                 dp[index1][index2] = max(include,max(exclude1,exclude2));
             }
         }
-
         return dp[n-1][m-1];
+    }
+
+    int solveOptm(vector<int>& nums1, vector<int>& nums2){
+        int n = nums1.size(), m=nums2.size();
+        vector<int> prev(m,0) , curr(m,0);
+
+        for(int index1=0;index1<n;index1++){
+            for(int index2=0;index2<m;index2++){
+                int include = (nums1[index1]*nums2[index2]);
+
+                if(index1>0 && index2>0) include += max(0,prev[index2-1]);
+                int exclude1 = INT_MIN, exclude2 = INT_MIN;
+                if(index1>=1) exclude1 = prev[index2];
+                if(index2>=1) exclude2 = curr[index2-1];
+
+                curr[index2] = max(include,max(exclude1,exclude2));
+            }
+
+            prev = curr;
+        }
+        return prev[m-1];
     }
 
 
@@ -38,6 +59,6 @@ public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m=nums2.size();
         
-        return solveTab(nums1,nums2);
+        return solveOptm(nums1,nums2);
     }
 };
