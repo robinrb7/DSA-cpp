21a97232b8999b94037712b21981798eb1e4ccdf
Robin Singh Khural <rkhural_be22@thapar.edu>
2026-01-08 21:24:14 +0530

    Time: 15 ms (76%), Space: 17.7 MB (54.8%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -13,13 +13,31 @@ private:
         return dp[index1][index2] = max(include,max(exclude1,exclude2));
     }
 
+    int solveTab(vector<int>& nums1, vector<int>& nums2){
+        int n = nums1.size(), m=nums2.size();
+        vector<vector<int>> dp(n,vector<int>(m,0));
+
+        for(int index1=0;index1<n;index1++){
+            for(int index2=0;index2<m;index2++){
+                int include = (nums1[index1]*nums2[index2]);
+
+                if(index1>0 && index2>0) include += max(0,solve(nums1,nums2,index1-1,index2-1,dp));
+                int exclude1 = 0 + solve(nums1,nums2,index1-1,index2,dp);
+                int exclude2 = 0 + solve(nums1,nums2,index1,index2-1,dp);
+
+                dp[index1][index2] = max(include,max(exclude1,exclude2));
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+
 
 
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m=nums2.size();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(nums1,nums2,n-1,m-1,dp);
+        return solveTab(nums1,nums2);
     }
 };
