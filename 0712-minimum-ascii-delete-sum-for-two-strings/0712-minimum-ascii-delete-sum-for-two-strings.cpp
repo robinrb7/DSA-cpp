12f06e2ef9b49389ca356d303c2042208c4d194c
Robin Singh Khural <rkhural_be22@thapar.edu>
2026-01-10 18:42:39 +0530

    Time: 3 ms (99.62%), Space: 9.7 MB (92.39%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -26,11 +26,78 @@ private:
 
         return dp[index1][index2] = min(include,min(exclude1,exclude2));
     }
+
+    int solveTab(string &s1, string &s2){
+        int n = s1.length(),m=s2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        int sum=0;
+        for(int i=1;i<=m;i++){
+            sum += s2[i-1];
+            dp[0][i] = sum;
+        }
+        sum=0;
+        for(int i=1;i<=n;i++){
+            sum += s1[i-1];
+            dp[i][0] = sum;
+        }
+
+        for(int index1=1;index1<=n;index1++){
+            for(int index2=1;index2<=m;index2++){
+
+                int include = INT_MAX, exclude1=INT_MAX, exclude2=INT_MAX;
+                if(s1[index1-1]==s2[index2-1]){
+                    include = dp[index1-1][index2-1];
+                }
+                else{
+                    exclude1 = (int)s1[index1-1] + dp[index1-1][index2];
+                    exclude2 = (int)s2[index2-1] + dp[index1][index2-1];
+                }
+
+                dp[index1][index2] = min(include,min(exclude1,exclude2));
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    int solveOptm(string &s1, string &s2){
+        int n = s1.length(),m=s2.length();
+        vector<int> prev(m+1,0) , curr(m+1,0);
+
+        int sum=0;
+        for(int i=1;i<=m;i++){
+            sum += s2[i-1];
+            prev[i] = sum;
+        }
+        
+
+        for(int index1=1;index1<=n;index1++){
+            curr[0] = prev[0] + s1[index1-1];
+            for(int index2=1;index2<=m;index2++){
+
+                int include = INT_MAX, exclude1=INT_MAX, exclude2=INT_MAX;
+                if(s1[index1-1]==s2[index2-1]){
+                    include = prev[index2-1];
+                }
+                else{
+                    exclude1 = (int)s1[index1-1] + prev[index2];
+                    exclude2 = (int)s2[index2-1] + curr[index2-1];
+                }
+
+                curr[index2] = min(include,min(exclude1,exclude2));
+            }
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+
+
 public:
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.length(),m=s2.length();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(s1,s2,n-1,m-1,dp);
+        return solveOptm(s1,s2);
     }
 };
