dc8187978764ab65d98baa88ffaa3d43eb6db65f
Robin Singh Khural <rkhural_be22@thapar.edu>
2026-01-19 21:48:37 +0530

    Time: 35 ms (28.64%), Space: 32.7 MB (60.91%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
-    bool possible(vector<vector<int>>& prefix,int threshold,int row,int col, int side){
-        int rowNo=row+side-1 , colNo = col+side-1;
+    bool possible(vector<vector<int>>& prefix,int threshold,int row,int col, int rowNo,int colNo){
+        
         int sum = prefix[rowNo][colNo];
         if(row>0) sum-= prefix[row-1][colNo];
         if(col>0)   sum-= prefix[rowNo][col-1];
@@ -23,20 +23,31 @@ public:
                 if(i>0 && j>0) prefix[i][j] -= prefix[i-1][j-1];
             }
         }
+        
+        
 
-        for(int side = maxSide;side>=1;side--){
-
-            for(int i=0;i+side-1<n;i++){
-                for(int j=0;j+side-1<m;j++){
+        
+        int ans = 0;
+        for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
                     
-                    if(possible(prefix,threshold,i,j,side)){
-                        return side;
+                    int low=1,high=maxSide;
+                    while(low<=high){
+                        int mid = low + (high-low)/2;
+
+                        int nRow = i + mid -1, nCol = j+mid-1;
+                        if(nRow<n && nCol<m && possible(prefix,threshold,i,j,nRow,nCol)){
+                            ans = max(ans,mid);
+                            low=mid+1;
+                        }
+                        else 
+                            high = mid-1;
+                        
                     }
 
                 }
             }
-        }
 
-        return 0;
+        return ans;
     }
 };
