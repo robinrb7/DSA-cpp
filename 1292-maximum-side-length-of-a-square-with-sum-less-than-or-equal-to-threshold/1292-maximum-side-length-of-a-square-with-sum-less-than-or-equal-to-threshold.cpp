30531bb3a6d1af2cc598940072fe72c72c177e65
Robin Singh Khural <rkhural_be22@thapar.edu>
2026-01-19 22:04:27 +0530

    Time: 20 ms (36.82%), Space: 32.8 MB (36.82%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,13 +1,20 @@
 class Solution {
 private:
-    bool possible(vector<vector<int>>& prefix,int threshold,int row,int col, int rowNo,int colNo){
+    bool possible(vector<vector<int>>& prefix,int threshold,int side, int n,int m){
 
-        int sum = prefix[rowNo][colNo];
-        if(row>0) sum-= prefix[row-1][colNo];
-        if(col>0)   sum-= prefix[rowNo][col-1];
-        if(row>0 && col>0)   sum+= prefix[row-1][col-1];
+        for(int row=0;row+side-1<n;row++){
+            for(int col=0;col+side-1<m;col++){
+                int rowNo = row+side-1, colNo = col+side-1;
 
-        return sum<=threshold;
+                int sum = prefix[rowNo][colNo];
+                if(row>0) sum-= prefix[row-1][colNo];
+                if(col>0)   sum-= prefix[rowNo][col-1];
+                if(row>0 && col>0)   sum+= prefix[row-1][col-1];
+
+                if(sum<=threshold) return true;
+            }
+        }
+        return false;
     }
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
@@ -22,31 +29,18 @@ public:
                 if(i>0 && j>0) prefix[i][j] -= prefix[i-1][j-1];
             }
         }
-        
-        
 
-        
-        int ans = 0;
-        for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    
-                    int low=1,high=min(n-i,m-j);
-                    while(low<=high){
-                        int mid = low + (high-low)/2;
+        int low=1,high=min(n,m);
+        while(low<=high){
+            int mid = low + (high-low)/2;
 
-                        int nRow = i + mid -1, nCol = j+mid-1;
-                        if(nRow<n && nCol<m && possible(prefix,threshold,i,j,nRow,nCol)){
-                            ans = max(ans,mid);
-                            low=mid+1;
-                        }
-                        else 
-                            high = mid-1;
-                        
-                    }
-
-                }
+            if(possible(prefix,threshold,mid,n,m)){
+                low=mid+1;
             }
+            else 
+                high = mid-1;
+        }
 
-        return ans;
+        return high;
     }
 };
