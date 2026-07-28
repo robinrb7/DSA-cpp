d138da26a9eb5a0996b249a6386121fd055845b7
Robin Singh Khural <robin7khural@gmail.com>
2026-07-29 00:29:01 +0530

    Time: 19 ms (91.21%), Space: 54 MB (95.38%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -5,16 +5,30 @@ public:
         int n=s.length();
         if(n==1) return s;
         
-        bool hasMiddle = n&1;
-        int firstHalfLen = n/2;
+        vector<int> count(26,0);
+        for(int i=0;i<n;i++){
+            count[s[i]-'a']++;
+        }
 
-        string firstHalf = s.substr(0,firstHalfLen);
-        sort(firstHalf.begin(),firstHalf.end());
-        string ans = firstHalf;
-        
-        reverse(firstHalf.begin(),firstHalf.end());
-    
-        ans =  (hasMiddle)? (ans + s[n/2] +  firstHalf) : (ans + firstHalf);
-        return ans;
+        char oddChar = '$';
+        int index=0;
+
+        for(int i=0;i<26;i++){
+            if(count[i]==0) continue;
+            if(count[i]&1) oddChar = i+'a';
+
+            int limit = count[i]/2;
+            char ch = i + 'a';
+            for(int j=index; j<(index + limit); j++){
+                s[j]= ch;
+                s[n-j-1]= ch;
+            }
+
+            index += limit;
+            if(index==n/2) break;
+        }
+
+        if(oddChar != '$') s[n/2] = oddChar;
+        return s;
     }
 };
