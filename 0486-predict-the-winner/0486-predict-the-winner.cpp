8d6fb3e53818c799ab64965b0799bc7160deb3d9
Robin Singh Khural <robin7khural@gmail.com>
2026-08-02 02:13:21 +0530

    Time: 95 ms (19.73%), Space: 10.2 MB (44.98%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -11,12 +11,26 @@ class Solution {
 
         return max(takeStart, takeEnd);
     }
+
+    int solveMem(vector<int>& nums, int start, int end,vector<vector<int>> &dp){
+        if(start==end) return nums[start];
+        
+        if(dp[start][end]!=-1) return dp[start][end];
+
+        int takeStart = nums[start] - solve(nums,start+1,end);
+        int takeEnd = nums[end] - solve(nums,start,end-1);
+
+        dp[start][end] = max(takeStart, takeEnd);
+        return dp[start][end];
+    }
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return true;
 
-        int playerFirstMaxScore = solve(nums,0,n-1);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+
+        int playerFirstMaxScore = solveMem(nums,0,n-1,dp);
         return (playerFirstMaxScore>=0) ;
     }
 };
