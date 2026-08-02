2bdf0908a15446d4ff80e1a1a86c73e630fe3a0a
Robin Singh Khural <robin7khural@gmail.com>
2026-08-02 19:27:53 +0530

    Time: 10 ms (28.51%), Space: 20 MB (7.11%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -3,33 +3,51 @@ class Solution {
     int solve(vector<int>& piles, int start, int end){
         if(start>end) return 0;
 
-        int takeStart = piles[start] - solve(piles,start+1,end);
-        int takeEnd = piles[end] - solve(piles,start,end-1);
+        int takeStart = piles[start] + min(solve(piles,start+2,end), solve(piles,start+1,end-1));
+        int takeEnd = piles[end] + min(solve(piles,start+1,end-1),solve(piles,start,end-2));
 
         return max(takeStart,takeEnd);
     }
 
-    int solveOptm(vector<int>& piles){
-        int n = piles.size();
-        vector<int> curr(n+1,0);
+    int solveMem(vector<int>& piles, int start, int end,vector<vector<int>> &dp){
+        if(start>end) return 0;
+        if(dp[start][end] != -1) return dp[start][end];
+
+        int takeStart = piles[start] + min(solveMem(piles,start+2,end,dp), solveMem(piles,start+1,end-1,dp));
+        int takeEnd = piles[end] + min(solveMem(piles,start+1,end-1,dp),solveMem(piles,start,end-2,dp));
+
+        return dp[start][end] = max(takeStart,takeEnd);
+    }
+
+    int solveTab(vector<int>& piles){
+        int n=piles.size();
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+
+        for(int i=0;i<n;i++) dp[i][i+2] = piles[i];
 
         for(int start=n-1;start>=0;start--){
-            curr[start+1] = piles[start];
-            for(int end=start+1;end<n;end++){
+            for(int end=start;end<n;end++){
+             //shift made in 'end' for handling indexing acc. to logic
 
-                int takeStart = piles[start] - curr[end+1];
-                int takeEnd = piles[end] - curr[end];
-                curr[end+1] = max(takeStart, takeEnd);
+                int takeStart = piles[start] + min(dp[start+2][end+2], dp[start+1][end+1]);
+                int takeEnd = piles[end] + min(dp[start+1][end+1],dp[start][end]);
+
+                dp[start][end+2] = max(takeStart,takeEnd);
             }
         }
-        
-        return curr[n];
+
+        return dp[0][n-1];
     }
+
+
+
+    
 public:
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
 
-        int playerFirstMaxScore = solveOptm(piles);
+        
+        int playerFirstMaxScore = solveTab(piles);
         return playerFirstMaxScore > 0;
     }
 };
