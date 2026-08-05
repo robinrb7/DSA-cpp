c33ddb34bbc338f54c861a6dd6e9e753c1a86283
Robin Singh Khural <robin7khural@gmail.com>
2026-08-05 23:23:48 +0530

    Time: 170 ms (74.14%), Space: 286.1 MB (99.31%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-        vector<int> adjList[n];
+        vector<int> adjList[n];     //(o(e))
         for(int i=0;i<invocations.size();i++){
             int u = invocations[i][0];
             int v = invocations[i][1];
@@ -12,19 +12,22 @@ public:
         vector<bool> malice(n,0);
         queue<int> q;
         q.push(k);
+        malice[k]=1;
         
-        while(!q.empty()){
+        while(!q.empty()){     //o(e+v)
             int maliceNode = q.front();
             q.pop();
-            malice[maliceNode]=1;
 
             for(int neigh: adjList[maliceNode]){
-                if(malice[neigh]==0) q.push(neigh);
+                if(malice[neigh]==0){
+                    malice[neigh]=1;
+                    q.push(neigh);
+                } 
             }
         }
 
         bool removeAllMaliceMethods= true;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){    //o(e+v)
             if(malice[i]) continue;
 
             for(int neigh: adjList[i]){
@@ -37,7 +40,7 @@ public:
         }
 
         vector<int> ans;
-        if(removeAllMaliceMethods){
+        if(removeAllMaliceMethods){   //o(v)
             for(int i=0;i<n;i++){
                 if(malice[i]==0) ans.push_back(i);
             } 
@@ -49,4 +52,6 @@ public:
         return ans;
 
     }
+
+    //tc -> 2(e+v)  ,Sc -> o(n) + o(n)
 };
