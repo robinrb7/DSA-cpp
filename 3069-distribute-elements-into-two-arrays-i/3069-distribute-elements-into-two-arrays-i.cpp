fb322702b4745fbd3143674d7c70014d98cf8d06
Robin Singh Khural <robin7khural@gmail.com>
2026-08-20 19:20:10 +0530

    Time: 3 ms (23.77%), Space: 24.1 MB (19.51%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return {nums[0]};
+
+        vector<int> arr1,arr2;
+        arr1.push_back(nums[0]), arr2.push_back(nums[1]);
+        for(int i=2;i<n;i++){
+            if(arr1.back()>arr2.back()) arr1.push_back(nums[i]);
+            else arr2.push_back(nums[i]);
+        }
+
+        vector<int> result;
+        int n1=arr1.size();
+        int i=0;
+        while(i<n1){
+            result.push_back(arr1[i]);
+            i++;
+        }
+
+        i=0;
+        int n2= arr2.size();
+        while(i<n2){
+            result.push_back(arr2[i]);
+            i++;
+        }
+
+        return result;
+    }
+};
